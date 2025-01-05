36b24b44c78b56d2f0af8d56f8067adac19cf050
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-06 02:15:03 +0530

    Time: 39 ms (43.99%) | Memory: 108.5 MB (21.51%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.cpp
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
     string shiftingLetters(string s, vector<vector<int>>& shifts) {
-        int n = s.size();
-        vector<int> diffArray(
-            n, 0);  // Initialize a difference array with all elements set to 0.
+        int n=s.size();
+        vector<int> diff(n,0);
 
-        for (auto shift : shifts) {
-            if (shift[2] == 1) {        // If direction is forward (1):
-                diffArray[shift[0]]++;  // Increment at the start index to
-                                        // indicate a forward shift.
-                if (shift[1] + 1 < n) {
-                    diffArray[shift[1] +
-                              1]--;  // Decrement at the end+1 index to stop the
-                                     // effect of the shift.
-                }
-            } else {                    // If direction is backward (0):
-                diffArray[shift[0]]--;  // Decrement at the start index to
-                                        // indicate a backward shift.
-                if (shift[1] + 1 < n) {
-                    diffArray[shift[1] +
-                              1]++;  // Increment at the end+1 index to stop the
-                                     // effect of the shift.
-                }
-            }
+        for(auto it:shifts)
+        {
+            diff[it[0]]+=(it[2]==1)?1:-1;
+            if(it[1]+1<n) diff[it[1]+1]-=(it[2]==1)?1:-1;
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            sum+=diff[i];
+            int netShift=sum;
+            if(netShift>=0) netShift%=26; 
+            else netShift=26-abs(netShift)%26; 
+            s[i]='a'+(s[i]-'a'+netShift)%26;
         }
 
-        string result(n, ' ');
-        int numberOfShifts = 0;
-
-        for (int i = 0; i < s.size(); i++) {
-            numberOfShifts = (numberOfShifts + diffArray[i]) %
-                             26;  // Update cumulative shifts, keeping within
-                                  // the alphabet range.
-            if (numberOfShifts < 0)
-                numberOfShifts +=
-                    26;  // Ensure `numberOfShifts` is non-negative.
-
-            // Calculate the new character by shifting `s[i]`
-            result[i] = 'a' + (s[i] - 'a' + numberOfShifts) % 26;
-        }
-
-        return result;
+        return s;
     }
 };
