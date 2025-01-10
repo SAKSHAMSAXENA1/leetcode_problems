43343273391089606b23a1340a40703e47d8c9d1
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-10 12:13:52 +0530

    Time: 392 ms (5.02%) | Memory: 188.6 MB (5.02%) - LeetSync

--- 952-word-subsets/word-subsets.cpp
+++ 952-word-subsets/word-subsets.cpp
@@ -13,10 +13,7 @@ public:
             {
                 temp[c]++;
                 b[c]=max(b[c],temp[c]);
-            }
-            
-
-            
+            }   
         }
 
         for(int i=0;i<n;i++)
