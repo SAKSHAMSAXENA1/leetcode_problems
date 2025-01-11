5f1083a24a2adad01e931f2734673fe76b139f30
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-11 17:13:54 +0530

    Time: 505 ms (58.98%) | Memory: 595.6 MB (31.40%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,69 @@
+struct Node {
+    Node *links[26];
+    bool endsWith=false;
+};
+class WordDictionary {
+public:
+    Node *root;
+    WordDictionary() {
+        root=new Node();
+    }
+    
+    void addWord(string word) {
+        Node *node=root;
+
+        for(auto c: word)
+        {
+            if(node->links[c-'a']==NULL)
+            {
+                node->links[c-'a']=new Node();
+            }
+            node=node->links[c-'a'];
+        }
+        node->endsWith=true;
+    }
+
+    bool f(string word, int index, Node *root)
+    {
+        int n=word.size();
+        if(index==n)
+        {
+            if(root && root->endsWith) return true;
+            return false;
+        }
+        
+        bool res=false;
+        Node *node=root;
+        for(int i=index;i<n;i++)
+        {
+            if(word[i]=='.')
+            {
+                for(char j='a';j<='z';j++)
+                {
+                    if(node->links[j-'a']!=NULL)
+                    res=res| f(word,i+1,node->links[j-'a']);
+                }
+
+                return res;
+            }
+            else if(node->links[word[i]-'a']==NULL) return false;
+            else
+            {
+                node=node->links[word[i]-'a'];
+            }
+        }
+
+        return node->endsWith;
+    }
+    
+    bool search(string word) {
+        return f(word,0,this->root);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
