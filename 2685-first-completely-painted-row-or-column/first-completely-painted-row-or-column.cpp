85bb7d7e5b3c6da1bc70209e1b2b4fe5b02b354f
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-20 13:43:21 +0530

    Time: 96 ms (62.50%) | Memory: 171 MB (37.80%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+       int n=mat.size(),m=mat[0].size(),k=arr.size();
+       unordered_map<int,pair<int,int>> u; // stores index of each element of matrix
+       for(int i=0;i<n;i++)
+       {
+        for(int j=0;j<m;j++)
+        {
+            u[mat[i][j]]={i,j};
+        }
+       }
+       vector<int> row(n,0),col(m,0); 
+
+       for(int i=0;i<k;i++)
+       {
+        int el=arr[i];
+        int elRow=u[el].first,elCol=u[el].second;
+        row[elRow]++;
+        if(row[elRow]==m) return i;
+
+        col[elCol]++;
+        if(col[elCol]==n) return i;
+       }
+
+       return -1;
+    }
+};
