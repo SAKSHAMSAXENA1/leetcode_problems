fbbb2122732adb7a727419866d8550f1e2520599
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-22 12:59:34 +0530

    Time: 31 ms (64.98%) | Memory: 31.8 MB (17.02%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -24,7 +24,23 @@ public:
     }
     bool isMatch(string s, string p) {
         int n=s.size(),m=p.size();
-        vector<vector<int>> dp(n+1,vector<int> (m+1, -1));
-        return f(n,m,s,p,dp);
+        vector<vector<int>> dp(n+1,vector<int> (m+1, false));
+        dp[0][0]=true;
+
+        // for(int i=1;i<=n;i++) dp[i][0]=false;
+        for(int j=1;j<=m && p[j-1]=='*';j++) dp[0][j]=true;
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==p[j-1]) dp[i][j]=dp[i-1][j-1];
+                else if(p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
+                else if(p[j-1]=='*') dp[i][j]=dp[i-1][j] || dp[i-1][j-1] || dp[i][j-1];
+                else dp[i][j]=false;
+            }
+        }
+
+        return dp[n][m];
     }
 };
