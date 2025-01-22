34e9c2aa23677826ae593f03cab6e5d92b96e9ea
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-22 13:05:34 +0530

    Time: 19 ms (86.99%) | Memory: 10.2 MB (86.63%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -24,23 +24,24 @@ public:
     }
     bool isMatch(string s, string p) {
         int n=s.size(),m=p.size();
-        vector<vector<bool>> dp(n+1,vector<bool> (m+1, false));
-        dp[0][0]=true;
+        vector<bool> prev(m+1, false),cur(m+1, false);
+        prev[0]=true;
 
-        // for(int i=1;i<=n;i++) dp[i][0]=false;
-        for(int j=1;j<=m && p[j-1]=='*';j++) dp[0][j]=true;
+        for(int j=1;j<=m && p[j-1]=='*';j++) prev[j]=true;
 
         for(int i=1;i<=n;i++)
         {
+            cur[0]=false;
             for(int j=1;j<=m;j++)
             {
-                if(s[i-1]==p[j-1]) dp[i][j]=dp[i-1][j-1];
-                else if(p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
-                else if(p[j-1]=='*') dp[i][j]=dp[i-1][j] || dp[i-1][j-1] || dp[i][j-1];
-                else dp[i][j]=false;
+                if(s[i-1]==p[j-1]) cur[j]=prev[j-1];
+                else if(p[j-1]=='?') cur[j]=prev[j-1];
+                else if(p[j-1]=='*') cur[j]=prev[j] || prev[j-1] || cur[j-1];
+                else cur[j]=false;
             }
+            prev=cur;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
