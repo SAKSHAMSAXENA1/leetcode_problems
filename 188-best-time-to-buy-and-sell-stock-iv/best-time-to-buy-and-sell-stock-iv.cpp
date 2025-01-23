b01bf65dee601e9a194e7762776e14019505fee3
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-23 18:41:17 +0530

    Time: 10 ms (50.87%) | Memory: 14.9 MB (75.25%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+int f(int i,int remTrx,int buy,vector<int>& prices,int n,vector<vector<vector<int>>> &dp)
+{
+    if(remTrx==0 || i==n) return 0;
+
+    if(dp[i][remTrx][buy]!=-1) return dp[i][remTrx][buy];
+
+    int profit=0;
+    if(buy)
+    {
+        profit=max(-prices[i]+f(i+1,remTrx,0,prices,n,dp),f(i+1,remTrx,1,prices,n,dp));
+    }
+    else
+    {
+        profit=max(prices[i]+f(i+1,remTrx-1,1,prices,n,dp),f(i+1,remTrx,0,prices,n,dp));
+    }
+
+    return dp[i][remTrx][buy]=profit;
+}
+    int maxProfit(int k, vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> ahead(k+1, vector<int> (2,0)),cur(k+1, vector<int> (2,0));
+
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int remTrx=1;remTrx<=k;remTrx++)
+            {
+                for(int buy=0;buy<=1;buy++)
+                {
+                    int profit=0;
+                    if(buy)
+                    {
+                        profit=max(-prices[i]+ahead[remTrx][0],ahead[remTrx][1]);
+                    }
+                    else
+                    {
+                        profit=max(prices[i]+ahead[remTrx-1][1],ahead[remTrx][0]);
+                    }
+
+                    cur[remTrx][buy]=profit;
+                }
+            }
+            ahead=cur;
+        }
+        return ahead[k][1];
+    }
+};
