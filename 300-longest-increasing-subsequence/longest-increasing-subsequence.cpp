0c69b3d3698ed707e335d0150f2f25a78e711d71
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-24 13:42:20 +0530

    Time: 66 ms (57.92%) | Memory: 14.2 MB (70.36%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
-int f(int i,int last,vector<int> &nums,int &n,vector<vector<int>> &dp)
-{
-    if(i==0) return 0;
-
-    if(dp[i][last]!=-1) return dp[i][last];
-
-    int take=0,notTake=0;
-    if(last==n+1 || nums[i-1]<nums[last-1])
-    {
-        take=1+f(i-1,i,nums,n,dp);
-    }
-    
-    notTake=f(i-1,last,nums,n,dp);
-    
-    return dp[i][last]=max(take,notTake);
-}
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> dp(n+2,vector<int> (n+2, 0));
+        int n=nums.size(),res=1;
+        vector<int> dp(n,1);
 
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<n;i++)
         {
-            for(int last=1;last<=n+1;last++)
+            for(int prev=0;prev<i;prev++)
             {
-                int take=0,notTake=0;
-                if(last==n+1 || nums[i-1]<nums[last-1])
+                if(nums[prev]<nums[i] && 1+dp[prev]>dp[i])
                 {
-                    take=1+dp[i-1][i];
+                    dp[i]=1+dp[prev];
                 }
-    
-                notTake=dp[i-1][last];
-    
-                dp[i][last]=max(take,notTake);
             }
+
+            res=max(res,dp[i]);
         }
-        return dp[n][n+1];
+
+        return res;
     }
 };
