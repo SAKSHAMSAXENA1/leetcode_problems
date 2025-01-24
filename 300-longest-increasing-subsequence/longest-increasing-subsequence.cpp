b7a9a3530a69bae09c4d1d3e90078533aee4005f
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-01-24 13:59:14 +0530

    Time: 87 ms (43.90%) | Memory: 14.4 MB (46.01%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size(),res=1;
-        vector<int> dp(n,1);
+        int n=nums.size(),res=1,lisIndex=0;
+        vector<int> dp(n,1),hash(n,0);
 
         for(int i=0;i<n;i++)
         {
+            hash[i]=i;
             for(int prev=0;prev<i;prev++)
             {
                 if(nums[prev]<nums[i] && 1+dp[prev]>dp[i])
                 {
                     dp[i]=1+dp[prev];
+                    hash[i]=prev;
                 }
             }
-
-            res=max(res,dp[i]);
+            if(dp[i]>res)
+            {
+                res=dp[i];
+                lisIndex=i;
+            }
+            //res=max(res,dp[i]);
         }
+
+        vector<int> temp;
+        int index=lisIndex;
+
+        while(hash[index]!=index)
+        {
+            temp.push_back(nums[index]);
+            index=hash[index];
+        }
+
+        temp.push_back(nums[index]);
+        reverse(temp.begin(),temp.end());
+
+        for(auto it:temp)
+        cout<<it<<" ";
+        cout<<endl;
 
         return res;
     }
