743f64f38600ecf8e5293acc4de62e867aaa137a
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-02-01 23:54:10 +0530

    Time: 8 ms (77.16%) | Memory: 51.2 MB (96.53%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
 bool dfsCheck(int node,vector<vector<int>> &graph,vector<bool> &vis,
-vector<bool> &visOnSamePath,vector<bool> &check)
+vector<bool> &visOnSamePath)
 {
     vis[node]=1;
     visOnSamePath[node]=1;
-    check[node]=0; // let us assume this node is not a safe node
+    //check[node]=0; // let us assume this node is not a safe node
 
     for(auto neighbour: graph[node])
     {
         if(!vis[neighbour])
         {
-            if(dfsCheck(neighbour,graph,vis,visOnSamePath,check)==true) 
+            if(dfsCheck(neighbour,graph,vis,visOnSamePath)==true) 
             return true; // this dfs call detects a cycle further down the recursion tree
         }
         else if(visOnSamePath[neighbour]) return true; // cycle is detected
     }
 
     // cycle is not detected
-    check[node]=1;
+    //check[node]=1;
     visOnSamePath[node]=0;
     return false;
 }
@@ -30,14 +30,14 @@ vector<bool> &visOnSamePath,vector<bool> &check)
         {
             if(!vis[i])
             {
-                dfsCheck(i,graph,vis,visOnSamePath,check);
+                dfsCheck(i,graph,vis,visOnSamePath);
             }
         }
 
         vector<int> safeNodes;
         for(int i=0;i<n;i++)
         {
-            if(check[i]) safeNodes.push_back(i);
+            if(!visOnSamePath[i]) safeNodes.push_back(i);
         }
 
         return safeNodes;
