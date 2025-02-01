731f8a79bacb18368bc86d7d97562df88a18d0c8
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-02-01 23:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 51.1 MB (96.53%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+bool dfsCheck(int node,vector<vector<int>> &graph,vector<bool> &vis,
+vector<bool> &visOnSamePath,vector<bool> &check)
+{
+    vis[node]=1;
+    visOnSamePath[node]=1;
+    check[node]=0; // let us assume this node is not a safe node
+
+    for(auto neighbour: graph[node])
+    {
+        if(!vis[neighbour])
+        {
+            if(dfsCheck(neighbour,graph,vis,visOnSamePath,check)==true) 
+            return true; // this dfs call detects a cycle further down the recursion tree
+        }
+        else if(visOnSamePath[neighbour]) return true; // cycle is detected
+    }
+
+    // cycle is not detected
+    check[node]=1;
+    visOnSamePath[node]=0;
+    return false;
+}
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<bool> vis(n,0),visOnSamePath(n,0),check(n,0);
+
+        for(int i=0;i<n;i++)
+        {
+            if(!vis[i])
+            {
+                dfsCheck(i,graph,vis,visOnSamePath,check);
+            }
+        }
+
+        vector<int> safeNodes;
+        for(int i=0;i<n;i++)
+        {
+            if(check[i]) safeNodes.push_back(i);
+        }
+
+        return safeNodes;
+    }
+};
