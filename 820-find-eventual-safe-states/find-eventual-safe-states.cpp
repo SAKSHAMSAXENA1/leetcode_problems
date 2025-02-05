61cbf977332538c8d5809f3d9e8a77fd0cc40527
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-02-05 12:53:32 +0530

    Time: 46 ms (53.62%) | Memory: 66.2 MB (41.57%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-bool dfsCheck(int node,vector<vector<int>> &graph,vector<bool> &vis,
-vector<bool> &visOnSamePath)
-{
-    vis[node]=1;
-    visOnSamePath[node]=1;
-    //check[node]=0; // let us assume this node is not a safe node
-
-    for(auto neighbour: graph[node])
-    {
-        if(!vis[neighbour])
-        {
-            if(dfsCheck(neighbour,graph,vis,visOnSamePath)==true) 
-            return true; // this dfs call detects a cycle further down the recursion tree
-        }
-        else if(visOnSamePath[neighbour]) return true; // cycle is detected
-    }
-
-    // cycle is not detected
-    //check[node]=1;
-    visOnSamePath[node]=0;
-    return false;
-}
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<bool> vis(n,0),visOnSamePath(n,0),check(n,0);
+        vector<int> indegree(n,0),safeNodes;
+        vector<vector<int>> adjReversed(n,vector<int>());
 
-        for(int i=0;i<n;i++)
+        for(int u=0;u<n;u++)
         {
-            if(!vis[i])
+            for(auto v: graph[u])
             {
-                dfsCheck(i,graph,vis,visOnSamePath);
+                adjReversed[v].push_back(u);
+                indegree[u]++;
             }
         }
 
-        vector<int> safeNodes;
-        for(int i=0;i<n;i++)
+        queue<int> q;
+        for(int node=0;node<n;node++)
         {
-            if(!visOnSamePath[i]) safeNodes.push_back(i);
+            if(indegree[node]==0) q.push(node);
         }
 
+        while(!q.empty())
+        {
+            int curNode=q.front();
+            q.pop();
+            safeNodes.push_back(curNode);
+
+            for(auto neighbour:adjReversed[curNode])
+            {
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0) q.push(neighbour);
+            }
+        }
+
+        sort(safeNodes.begin(),safeNodes.end());
         return safeNodes;
     }
 };
