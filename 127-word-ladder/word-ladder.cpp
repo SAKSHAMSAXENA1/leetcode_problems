4f86223fcd83a7b0415b0502458a6087b19d8cfa
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-02-09 13:16:03 +0530

    Time: 55 ms (77.91%) | Memory: 21.2 MB (47.65%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,91 +1,39 @@
 class Solution {
 public:
-bool isNeighbour(string &a,string &b)
-{
-    int m=a.size();
-    if(m!=b.size()) return false;
-    int cntMismatch=0;
-    for(int i=0;i<m;i++)
-    {
-        if(a[i]!=b[i]) cntMismatch++;
-
-        if(cntMismatch>1) return false;
-    }
-
-    return true;
-}
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        bool flag=false;
-        for(auto it:wordList)
-        {
-            if(it==endWord) 
-            {
-                flag=true;
-                break;
-            }
-        }
-        if(flag==false) return 0;
-
-        bool shouldInsert=true;
-        int src=0,n=wordList.size();
-        for(int i=0;i<n;i++)
-        {
-            if(wordList[i]==beginWord) 
-            {
-                shouldInsert=false;
-                src=i;
-                break;
-            }
-        }
-        if(shouldInsert) 
-        {
-            wordList.insert(wordList.begin(),beginWord);
-            n++;
-        }
-
-        vector<vector<int>> adj(n, vector<int>());
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                //if(i==j) continue;
-
-                if(isNeighbour(wordList[i], wordList[j]))
-                {
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
-        }
-        vector<int> dist(n , 1e9);
-        dist[src]=0;
-        queue<int> q;
-        q.push(src);
+        unordered_set<string> st(wordList.begin(),wordList.end());
+        queue<pair<string,int>> q;
+        int wordLength=beginWord.size();
+        q.push({beginWord,1});
+        st.erase(beginWord);
 
         while(!q.empty())
         {
-            int curNode=q.front();
+            string curWord=q.front().first;
+            int curLevel=q.front().second;
             q.pop();
 
-            for(auto neighbour: adj[curNode])
+            if(curWord==endWord) return curLevel;
+
+            for(int i=0;i<wordLength;i++)
             {
-                if(dist[curNode]+1<dist[neighbour])
+                char original=curWord[i];
+
+                for(char c='a';c<='z';c++)
                 {
-                    dist[neighbour]=dist[curNode]+1;
-                    q.push(neighbour);
+                    curWord[i]=c;
+
+                    if(st.find(curWord)!=st.end())
+                    {
+                        q.push({curWord,curLevel+1});
+                        st.erase(curWord);
+                    }
                 }
-            }
-        }
-        int res=0;
-        for(int i=0;i<n;i++)
-        {
-            if(wordList[i]==endWord && dist[i]!=1e9)
-            {
-                res=dist[i]+1;
-                break;
+
+                curWord[i]=original;
             }
         }
 
-        return res;
+    return 0;
     }
 };
