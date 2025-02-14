d201f37c17274aac7e3435ce04e0227fa445f410
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-02-15 01:21:39 +0530

    Time: 14 ms (87.83%) | Memory: 77.8 MB (93.39%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.cpp
@@ -1,21 +1,27 @@
 class ProductOfNumbers {
 public:
 vector<int> prefixProduct;
+int n; // size of prefixProduct array
     ProductOfNumbers() {
         prefixProduct.push_back(1);
+        n=1;
     }
     
     void add(int num) {
-        if(num) prefixProduct.push_back(prefixProduct.back()*num);
+        if(num) 
+        {
+            prefixProduct.push_back(prefixProduct[n-1]*num);
+            n++;
+        }
         else 
         {
             prefixProduct.clear();
             prefixProduct.push_back(1);
+            n=1;
         }
     }
     
     int getProduct(int k) {
-        int  n=prefixProduct.size();
         if(n-k-1<0) return 0;
         return prefixProduct[n-1]/prefixProduct[n-k-1];
     }
