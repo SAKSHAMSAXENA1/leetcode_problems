7c2a6b451055681d96fcf580854820133970c003
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-02-15 23:22:50 +0530

    Time: 12 ms (74.79%) | Memory: 17.2 MB (27.27%) - LeetSync

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,53 +1,39 @@
 class Solution {
 public:
-int f(int &src,vector<vector<pair<int,int>>> &adj, int &distanceThreshold,int &n)
-{
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
-    vector<int> dist(n,1e9);
-    pq.push({0,src}); // {dist,node}
-    dist[src]=0;
-    
-    while(!pq.empty())
-    {
-        int curDist=pq.top().first,curNode=pq.top().second;
-        pq.pop();
-
-        for(auto it:adj[curNode])
-        {
-            int neighbour=it.first,edgeWt=it.second;
-            if(curDist+edgeWt<dist[neighbour])
-            {
-                dist[neighbour]=curDist+edgeWt;
-                pq.push({dist[neighbour],neighbour});
-            }
-        }
-    }
-    int cnt=0;
-    for(auto it:dist)
-    {
-        if(it<=distanceThreshold) cnt++;
-    }
-
-    return cnt;
-}
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<pair<int,int>>> adj(n,vector<pair<int,int>>());
+        vector<vector<int>> dist(n,vector<int>(n,1e9));
 
         for(auto it:edges)
         {
-            int u=it[0],v=it[1],wt=it[2];
-            adj[u].push_back({v,wt});
-            adj[v].push_back({u,wt});
+            int u=it[0],v=it[1],edgeWt=it[2];
+            dist[u][v]=dist[v][u]=edgeWt;
         }
 
+        for(int i=0;i<n;i++) dist[i][i]=0;
+
+        for(int via=0;via<n;via++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    dist[i][j]=min(dist[i][j],dist[i][via]+dist[via][j]);
+                }
+            }
+        }
         int minNeighbours=n,res=-1;
         for(int i=0;i<n;i++)
         {
-            int temp=f(i,adj,distanceThreshold,n);
-            if(temp<=minNeighbours) 
+            int cnt=0;
+            for(auto it:dist[i])
             {
+                if(it<=distanceThreshold) cnt++;
+            }
+
+            if(cnt<=minNeighbours)
+            {
+                minNeighbours=cnt;
                 res=i;
-                minNeighbours=temp;
             }
         }
 
