2f8cd6ed36b246e7f8eb9e03c86e701f5568847b
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-02-18 17:51:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (65.36%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,29 +1,79 @@
+class DisjointSet{
+    vector<int> size,parent,rank;
+    public:
+    DisjointSet(int n)
+    {
+        size.resize(n+1,1);
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++) 
+        parent[i]=i;
+    }
+
+    int findUPar(int node)
+    {
+        if(node==parent[node]) return node;
+        //else
+        return parent[node]=findUPar(parent[node]);
+    }
+
+    void unionBySize(int u,int v)
+    {
+        int ulp_u=findUPar(u),ulp_v=findUPar(v);
+        if(size[ulp_u]<size[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+        }
+    }
+
+    void unionByRank(int u,int v)
+    {
+        int ulp_u=findUPar(u),ulp_v=findUPar(v);
+        if(rank[ulp_u]<rank[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+        }
+        else if(rank[ulp_v]<rank[ulp_u])
+        {
+            parent[ulp_v]=ulp_u;
+        }
+        else // rank[ulp_u]==rank[ulp_v]
+        {
+            parent[ulp_u]=ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+};
 class Solution {
 public:
-void dfs(int node, vector<vector<int>> &adj,vector<bool> &vis,int &n)
-{
-    vis[node]=true;
-
-    for(int i=0;i<n;i++)
-    {
-        if(i==node) continue;
-
-        if(adj[node][i]==1 && !vis[i]) dfs(i,adj,vis,n);
-    }
-}
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int res=0,n=isConnected.size();
-        vector<bool> vis(n,false);
+        int n=isConnected.size();
+        DisjointSet ds(n);
 
         for(int i=0;i<n;i++)
         {
-            if(!vis[i])
+            for(int j=i+1;j<n;j++)
             {
-                dfs(i,isConnected,vis,n);
-                res++;
+                if(isConnected[i][j])
+                {
+                    ds.unionByRank(i,j);
+                }
             }
         }
+        unordered_set<int> uniqueParents;// each component has a unique parent
+        // so no of unique parents=no of components
 
-        return res;
+        for(int i=0;i<n;i++)
+        {
+            uniqueParents.insert(ds.findUPar(i));
+        }
+
+        return uniqueParents.size();
     }
 };
