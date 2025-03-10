2c77f53e105034e8b1ef82e60fc94d713769080d
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-03-10 13:55:35 +0530

    Time: 95 ms (77.79%) | Memory: 13.8 MB (57.17%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -22,8 +22,33 @@ int f(int i,int j,vector<int> &nums,vector<vector<int>> &dp)
         int n=nums.size();
         nums.insert(nums.begin(),1); // padding
         nums.push_back(1); // padding
+        int m=n+2; // new size after padding 1s on extreme left and right
         
-        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
-        return f(0,n+1,nums,dp);
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
+
+        // for(int j=1;j<m;j++) dp[j-1][j]=0; // base case-> 0 balloon to burst
+
+    // to have atleast one balloon to burst in the interval, iMAX=n-1, since j>=i+2 and j<=n+1
+        // however even if we don't think this deep just start from i=n+1 or n, it will still 
+        // work
+        for(int i=n-1;i>=0;i--)
+        {
+            // start from the case just after base case (j=i+1), 
+            // i.e j=i+2 onwards (1 balloon to burst)
+            for(int j=i+2;j<m;j++)
+            {
+                int maxi=INT_MIN;
+                for(int k=i+1;k<j;k++)
+                {
+                    int cost=nums[i]*nums[k]*nums[j] // coins collected by bursting this balloon
+                                +dp[i][k] // coins collected from left subproblem/interval
+                                +dp[k][j]; // coins collected from right subproblem/interval
+
+                     maxi=max(maxi,cost);
+                }
+                dp[i][j]=maxi;
+            }
+        }
+        return dp[0][n+1];
     }
 };
