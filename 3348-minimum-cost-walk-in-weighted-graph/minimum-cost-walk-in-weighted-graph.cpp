e9986155fc75da3118c9db2e7063ef3551ac18d0
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-03-20 15:38:17 +0530

    Time: 174 ms (26.30%) | Memory: 243.3 MB (32.47%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.cpp
@@ -1,55 +1,76 @@
-class Solution {
+class DisjointSet{
+    vector<int> size,parent;
+    unordered_map<int,int> compAndValue;
 public:
-void dfs(int &node,vector<vector<int>> &adj,vector<bool> &vis,vector<int> &comp,int &componentNo)
-{
-    vis[node]=true;
-    comp[node]=componentNo;
-
-    for(auto neighbour: adj[node])
+    DisjointSet(int n)
     {
-        if(!vis[neighbour])
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++) parent[i]=i;
+    }
+
+    int findUPar(int node)
+    {
+        if(node==parent[node]) return node;
+        return parent[node]=findUPar(parent[node]);
+    }
+
+    void unionBySize(int u,int v,int wt)
+    {
+        int ulp_u=findUPar(u),ulp_v=findUPar(v);
+
+        if(compAndValue.find(ulp_u)==compAndValue.end())
+        compAndValue[ulp_u]=INT_MAX;
+
+        if(compAndValue.find(ulp_v)==compAndValue.end())
+        compAndValue[ulp_v]=INT_MAX;
+
+        if(ulp_u==ulp_v) // both u & v belong to the same component
         {
-            dfs(neighbour,adj,vis,comp,componentNo);
+            compAndValue[ulp_u]&=wt;
+            return; 
+        } 
+
+        if(size[ulp_u]<size[ulp_v])
+        {
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+            compAndValue[ulp_v]&=compAndValue[ulp_u] & wt;
+        }
+        else
+        {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
+            compAndValue[ulp_u]&=compAndValue[ulp_v] & wt;
         }
     }
-}
+
+    int andVal(int u)
+    {
+        return compAndValue[u];
+    }
+};
+class Solution {
+public:
     vector<int> minimumCost(int n, vector<vector<int>>& edges, vector<vector<int>>& query) {
-        vector<bool> vis(n,false);
-        vector<int> comp(n),res;
-        unordered_map<int,int> compAndValue;
-        vector<vector<int>> adj(n);
+        DisjointSet ds(n);
+        vector<int> res;
         for(auto it:edges)
         {
-            int u=it[0],v=it[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
-        int componentNo=1;
-
-        for(int i=0;i<n;i++)
-        {
-            if(!vis[i])
-            {
-                dfs(i,adj,vis,comp,componentNo);
-                compAndValue[componentNo]=INT_MAX;
-                componentNo++;
-            }
-        }
-
-        for(auto it:edges)
-        {
-            compAndValue[comp[it[0]]]&=it[2]; // it=[u,v,wt] 
+            int u=it[0],v=it[1],wt=it[2]; // it=[u,v,wt]
+            ds.unionBySize(u,v,wt);
         }
 
         for(auto it:query)
         {
             int u=it[0],v=it[1];
+            int ulp_u=ds.findUPar(u),ulp_v=ds.findUPar(v);
             if(u==v)
                 res.push_back(0); 
-            else if(comp[u]!=comp[v])
+            else if(ulp_u!=ulp_v)
                 res.push_back(-1);
             else
-                res.push_back(compAndValue[comp[u]]);
+                res.push_back(ds.andVal(ulp_u));
         }
 
         return res;
