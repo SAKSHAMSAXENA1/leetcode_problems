4f15ad3cd83030450112a06f156bd4c22b61512c
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-03-21 13:10:36 +0530

    Time: 33 ms (12.01%) | Memory: 11.5 MB (63.28%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-bool windowContainsT(unordered_map<char,int> &uS,unordered_map<char,int> &uT)
+bool windowContainsT(int uS[],int uT[])
 {
-    for(auto it:uT)
+    for(int i=0;i<52;i++)
     {
-        // it=<char,freq>
-        if(uS[it.first]<it.second) return false;
+        if(uS[i]<uT[i]) return false;
     }
     return true;
 }
     string minWindow(string s, string t) {
-        unordered_map<char,int> uT,uS;
+        int uS[52]={0},uT[52]={0};
         int l=0,r=0,n=s.size(),minLen=INT_MAX;
         int ansL=-1,ansR=-1;
 
-        for(auto it:t) uT[it]++;
+        for(auto it:t)
+        {
+            if('a'<=it) uT[it-'a'+26]++;
+            else uT[it-'A']++;
+        }
 
         while(r<n)
         {
-            uS[s[r]]++; // (acquire)
+            if('a'<=s[r]) uS[s[r]-'a'+26]++; // (acquire)
+            else uS[s[r]-'A']++; // (acquire)
 
             while(windowContainsT(uS,uT))
             {
@@ -30,7 +34,8 @@ bool windowContainsT(unordered_map<char,int> &uS,unordered_map<char,int> &uT)
                 }
                 //minLen=min(minLen,r-l+1);
                 
-                uS[s[l]]--; // (release)
+                if('a'<=s[l]) uS[s[l]-'a'+26]--; // (release)
+                else uS[s[l]-'A']--; // (release)
                 l++; // shrink the window (release)
             }
 
