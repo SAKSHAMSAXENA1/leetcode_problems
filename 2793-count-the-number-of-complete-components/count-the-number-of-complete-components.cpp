8393d7dad87af567b20aaeee30d698583968461b
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-03-22 18:58:56 +0530

    Time: 62 ms (45.06%) | Memory: 134.7 MB (48.03%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -1,58 +1,40 @@
 class Solution {
 public:
-void bfs(int &node,vector<vector<int>> &adj,vector<bool> &vis,vector<int> &component,
-int &componentIndex)
+void dfs(int &node,vector<vector<int>> &adj,vector<bool> &vis,int &nodesCnt,int &totOutdegree)
 {
     vis[node]=true;
-    component[node]=componentIndex;
-    queue<int> q;
-    q.push(node);
-    
-    while(!q.empty())
-    {
-        int curNode=q.front();
-        q.pop();
+    totOutdegree+=adj[node].size();
+    nodesCnt++;
 
-        for(auto neighbour:adj[curNode])
+    for(auto neighbour:adj[node])
+    {
+        if(!vis[neighbour])
         {
-            if(!vis[neighbour])
-            {
-                vis[neighbour]=true;
-                q.push(neighbour);
-                component[neighbour]=componentIndex;
-            }
+            dfs(neighbour,adj,vis,nodesCnt,totOutdegree);
         }
     }
 }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<int> component(n,0),componentSize(n,0);
-        vector<vector<int>> adj(n,vector<int>());
         vector<bool> vis(n,false);
-        unordered_set<int> incompleteComponents;
+        vector<vector<int>> adj(n,vector<int>());
+        int cnt=0;
         for(auto it:edges)
         {
             int u=it[0],v=it[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        int componentIndex=0;
+
         for(int i=0;i<n;i++)
         {
             if(!vis[i])
             {
-                bfs(i,adj,vis,component,componentIndex);
-                componentIndex++;
+                int nodesCnt=0,totOutdegree=0;
+                dfs(i,adj,vis,nodesCnt,totOutdegree);
+                if(totOutdegree==nodesCnt*(nodesCnt-1)) cnt++;
             }
-    
-            componentSize[component[i]]++;
         }
 
-        for(int i=0;i<n;i++)
-        {
-            if(adj[i].size()!=componentSize[component[i]]-1)
-            incompleteComponents.insert(component[i]);
-        }
-        
-        return componentIndex-incompleteComponents.size();
+        return cnt;
     }
 };
