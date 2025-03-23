c9e49f214c6513de1a6581ebd2d1ecb6086174af
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-03-23 23:13:58 +0530

    Time: 64 ms (5.29%) | Memory: 53.6 MB (5.54%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,44 +1,45 @@
+#define mod (int)(1e9+7)
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adj(n,vector<pair<int,int>>());
-        for(auto it:roads)
+        priority_queue<pair<long,int>,vector<pair<long,int>>,greater<>> pq;
+        vector<vector<vector<int>>> adj(n,vector<vector<int>>());
+        for(auto edge:roads)
         {
-            int u=it[0],v=it[1],edgeWt=it[2];
-            adj[u].push_back({v,edgeWt});
-            adj[v].push_back({u,edgeWt});
+            int u=edge[0],v=edge[1],wt=edge[2];
+            adj[u].push_back({v,wt}); // {adjacent node, wt}
+            adj[v].push_back({u,wt});
         }
-        vector<long long> dist(n,1e18),noOfWays(n,0);
-        int mod=1e9+7;
-        dist[0]=0;
-        noOfWays[0]=1;
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<>> pq;
-        pq.push({0,0}); 
+        vector<long> dist(n,LONG_MAX); // all nodes unreachable, initially
+        vector<int> cnt(n,1); // only 1 way to reach each node, initially
+        dist[0]=0; // dist[src]=0
+        pq.push({0,0}); // {dist,node}
 
         while(!pq.empty())
         {
-            long long curDist=pq.top().first;
             int curNode=pq.top().second;
+            long curDist=pq.top().first;
             pq.pop();
 
-            for(auto edge:adj[curNode])
+            for(auto it:adj[curNode])
             {
-                int neighbour=edge.first,edgeWeight=edge.second;
-
-                if(curDist+edgeWeight<dist[neighbour])
+                int neighbour=it[0],wt=it[1];
+                if(curDist+(long)wt<dist[neighbour])
                 {
-                    dist[neighbour]=curDist+edgeWeight;
-                    noOfWays[neighbour]=noOfWays[curNode];
+                    dist[neighbour]=curDist+wt;
+                    cnt[neighbour]=cnt[curNode]; 
+                    // ways to reach neighbour with min dist=ways to reach curNode (adopt)
 
-                    if(neighbour!=n-1) pq.push({dist[neighbour],neighbour});
+                    pq.push({dist[neighbour],neighbour});
                 }
-                else if(curDist+edgeWeight==dist[neighbour])
+                else if(curDist+wt==dist[neighbour])
                 {
-                    noOfWays[neighbour]=(noOfWays[neighbour]+noOfWays[curNode])%mod;
+                    cnt[neighbour]=(cnt[neighbour]%mod+cnt[curNode]%mod)%mod;
+                    // ways to reach neighbour with min dist + =ways to reach curNode (add)
                 }
             }
         }
 
-        return noOfWays[n-1];
+        return cnt[n-1];
     }
 };
