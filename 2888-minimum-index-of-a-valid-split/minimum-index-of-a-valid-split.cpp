53ecfca1b0adfc7893f8c171c52985e32a3ff4bf
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-03-27 18:48:44 +0530

    Time: 53 ms (32.38%) | Memory: 103 MB (19.26%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
+        unordered_map<int,int> map1,map2;
         int n=nums.size();
-        unordered_map<int,int> u;
-        vector<int> dominantInSuffix(n,-1);
 
         for(int j=n-1;j>=0;j--)
+        map2[nums[j]]++;
+
+        for(int i=0;i<n;i++)
         {
-            u[nums[j]]++;
+            map1[nums[i]]++; // inclusion of nums[i] in left subarray->map1
+            map2[nums[i]]--; // removal of nums[i] from right subarray->map2
 
-            if(u[nums[j]]*2>(n-j))
-            dominantInSuffix[j]=nums[j];
-            else if(j+1<n && u[dominantInSuffix[j+1]]*2>(n-j))
-            dominantInSuffix[j]=dominantInSuffix[j+1];
-        }
-
-        u.clear();
-
-        for(int i=0;i<n-1;i++)
-        {
-            u[nums[i]]++;
-
-            if(u[nums[i]]*2>(i+1)) // if nums[i] is dominant in range [0...i]
-            {
-                if(nums[i]==dominantInSuffix[i+1]) //if nums[i] is also dominant in [i+1...n-1]
-                return i;
-            }
+            int n1=i+1,n2=n-i-1;
+            if(map1[nums[i]]*2>n1 && map2[nums[i]]*2>n2)
+            return i;
         }
 
         return -1;
