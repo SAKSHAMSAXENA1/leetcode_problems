08e9c3b044281b76eb54d39c7346c924e3c584aa
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-03-30 13:23:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (97.09%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -2,41 +2,41 @@ class Solution {
 public:
     vector<int> partitionLabels(string s) {
         int n=s.size();
-        vector<vector<int>> intervals(26, vector<int>(2,-1));
+        vector<pair<int,int>> intervals(26, {-1,-1});
         vector<int> res;
 
         for(int i=0;i<n;i++)
         {
-            if(intervals[s[i]-'a'][0]==-1) // this char is seen for the first time
+            if(intervals[s[i]-'a'].first==-1) // this char is seen for the first time
             {
                 // update its both first and last indices
-                intervals[s[i]-'a'][0]=intervals[s[i]-'a'][1]=i;
+                intervals[s[i]-'a'].first=intervals[s[i]-'a'].second=i;
             }
             else // this char has been seen previously, so update its last index only
-            intervals[s[i]-'a'][1]=i;
+            intervals[s[i]-'a'].second=i;
         }
 
         sort(intervals.begin(),intervals.end());
-        vector<vector<int>> merged;
+        vector<pair<int,int>> merged;
         int i=0;
         for(;i<26;i++)
         {
-            if(intervals[i][0]!=-1) break;
+            if(intervals[i].first!=-1) break;
         }
         merged.push_back(intervals[i]);
         i++;
 
         for(;i<26;i++)
         {
-            if(intervals[i][0]<merged.back()[1]) // merging intervals
-            merged.back()[1]=max(merged.back()[1],intervals[i][1]);
+            if(intervals[i].first<merged.back().second) // merging intervals
+            merged.back().second=max(merged.back().second,intervals[i].second);
             else
             merged.push_back(intervals[i]);
         }
 
         for(auto it:merged)
         {
-            res.push_back(it[1]-it[0]+1);
+            res.push_back(it.second-it.first+1);
         }
 
         return res;
