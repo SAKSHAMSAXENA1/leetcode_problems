e0f0c128ac9f924e40800dfd010a17bc1241d18a
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-03 01:08:30 +0530

    Time: 3 ms (62.91%) | Memory: 10.9 MB (95.28%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,45 +1,38 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        // stack is monotonic increasing-> last digits will be higher than prev digits
-        stack<char> st;
-        string res="";
+        // string also works like stack, so use it directly without using temporary stack
+        // res is monotonic increasing, similar to monotonic increasing stack
+        string res="";  
+         
 
         for(auto it:num)
         {
-            // while the stack has higher digits in it and you have the freedom to delete
-            //, pop out digits/char from stack 
-            while(!st.empty() && st.top()>it && k>0)
+            // while res already has higher digits and i have freedom to delete digits,
+            // keep popping them from res
+            while(res.size() && res.back()>it && k>0)
             {
-                st.pop(); // remove higher digits from stack
-                k--; // decrease the freedom to delete char
+                res.pop_back();
+                k--;
             }
 
-            // if the stack already has some elements, then push 'it' without any concern
-            // otherwise, if the stack is empty, then push only non-zero digits into it
-            // to avoid issue of leading/preceding zeros
-            if(st.size() || it!='0')
-            st.push(it);  
+            // if res is non-empty, then push the current digit without any concern,
+            // otherwise if res is empty then push the current digit only if it is non-zero
+            // to avoid the issue of leading/preceding zeros
+            if(res.size() || it!='0')
+            res.push_back(it);
         }
 
-        // while you still have freedom to delete digits, pop out the last digits
-        // since stack is montonic increasing, the later digits will be higher than prev digits
-        while(st.size() && k>0) 
+        // while i still have freedom to delete char, remove the ones at the back/last
+        // since res is monotonic increasing, the later digits are of higher val than prev digits
+        while(res.size() && k>0)
         {
-            st.pop();
+            res.pop_back();
             k--;
         }
 
-        while(!st.empty())
-        {
-            res.push_back(st.top());
-            st.pop();
-        }
-
-        reverse(res.begin(),res.end());
         if(res=="") return "0";
         return res;
-
 
     }
 };
