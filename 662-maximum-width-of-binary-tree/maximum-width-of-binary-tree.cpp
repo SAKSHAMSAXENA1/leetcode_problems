81c0154cfc5ea556653e5f65c77225fbc7aa84ed
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-13 16:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (35.04%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -4,32 +4,32 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode() : val(0), left(nulptr), right(nulptr) {}
+ *     TreeNode(int x) : val(x), left(nulptr), right(nulptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 
-#define ll long long
+#define l long
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode *,ll>> q; //(node,index)
+        queue<pair<TreeNode *,l>> q; //(node,index)
         q.push({root,0});
-        ll res=0;
+        l res=0;
 
         while(!q.empty())
         {
             int n=q.size(),referenceIndex=q.front().second;
-            ll firstVal=q.front().second,lastVal=q.back().second;
+            l firstVal=q.front().second,lastVal=q.back().second;
             res=max(res,lastVal-firstVal+1);
 
             for(int i=0;i<n;i++)
             {
                 auto node=q.front().first;
-                ll absoluteIndex=q.front().second;
+                l absoluteIndex=q.front().second;
                 q.pop();
-                ll relativeIndex=absoluteIndex-referenceIndex;
+                l relativeIndex=absoluteIndex-referenceIndex;
 
                 if(node->left) q.push({node->left,2*relativeIndex+1});
                 if(node->right) q.push({node->right,2*relativeIndex+2});
