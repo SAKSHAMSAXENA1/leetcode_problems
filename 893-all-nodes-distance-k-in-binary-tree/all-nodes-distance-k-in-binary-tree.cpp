19bd64032d35ac4f050520582f95ec705e44f188
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-14 22:43:43 +0530

    Time: 3 ms (82.34%) | Memory: 16.7 MB (8.39%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,36 +9,34 @@
  */
 class Solution {
 public:
-void preOrder(TreeNode *root,unordered_map<int,vector<int>> &adj, unordered_map<int,bool> &vis)
+void preOrder(TreeNode *root,unordered_map<TreeNode *,TreeNode *> &parent,
+unordered_map<TreeNode *,bool> &vis)
 {
-    vis[root->val]=false;
+    if(root==NULL) return;
 
+    vis[root]=false;
     if(root->left)
     {
-        adj[root->val].push_back(root->left->val);
-        adj[root->left->val].push_back(root->val);
-        preOrder(root->left,adj,vis);
+        parent[root->left]=root;
+        preOrder(root->left,parent,vis);
     }
 
     if(root->right)
     {
-        adj[root->val].push_back(root->right->val);
-        adj[root->right->val].push_back(root->val);
-        preOrder(root->right,adj,vis);
+        parent[root->right]=root;
+        preOrder(root->right,parent,vis);
     }
 }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        unordered_map<int,vector<int>> adj;
-        unordered_map<int,bool> vis;
-        //vector<bool> vis(adj.size(), false);
+        unordered_map<TreeNode *,TreeNode *> parent;
+        unordered_map<TreeNode *,bool> vis;
         vector<int> res;
-        preOrder(root,adj,vis);
-        
-        queue<int> q;
-        q.push(target->val); // start bfs from the target node
-        vis[target->val]=true;
-        // when you process nodes of level 'i', you insert nodes of level 'i+1' into the queue
-        int curLevel=0; 
+        preOrder(root,parent,vis);
+
+        queue<TreeNode *> q;
+        q.push(target);
+        vis[target]=true;
+        int curLevel=0;
 
         while(!q.empty() && curLevel<k)
         {
@@ -46,29 +44,35 @@ void preOrder(TreeNode *root,unordered_map<int,vector<int>> &adj, unordered_map<
 
             for(int i=0;i<n;i++)
             {
-                int curNode=q.front();
+                auto curNode=q.front();
                 q.pop();
 
-                for(auto neighbour:adj[curNode])
+                if(curNode->left && !vis[curNode->left])
                 {
-                    if(!vis[neighbour])
-                    {
-                        q.push(neighbour);
-                        vis[neighbour]=true;
-                    }
+                    q.push(curNode->left);
+                    vis[curNode->left]=true;
+                }
+
+                if(curNode->right && !vis[curNode->right])
+                {
+                    q.push(curNode->right);
+                    vis[curNode->right]=true;
+                }
+
+                if(parent.find(curNode)!=parent.end() && !vis[parent[curNode]])
+                {
+                    q.push(parent[curNode]);
+                    vis[parent[curNode]]=true;
                 }
             }
 
             curLevel++;
         }
 
-        if(curLevel==k)
+        while(!q.empty())
         {
-            while(!q.empty())
-            {
-                res.push_back(q.front());
-                q.pop();
-            }
+            res.push_back(q.front()->val);
+            q.pop();
         }
 
         return res;
