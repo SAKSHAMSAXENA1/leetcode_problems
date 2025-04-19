9ff860a07ad569e1dacba21c44eddf850f78a04e
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-19 19:21:52 +0530

    Time: 4 ms (56.34%) | Memory: 9.7 MB (89.22%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string prev,cur;
+        string prev;
         prev="1"; // base case
 
         for(int k=2;k<=n;k++)
         {
-            string s=prev;
-            string res=""; // res is rle
-            int m=s.size();
+            string res=""; // res is rle, cur=res
+            int m=prev.size();
 
             for(int i=0;i<m;i++)
             {
                 int j=i+1;
-                while(j<m && s[i]==s[j])
+                while(j<m && prev[i]==prev[j])
                 j++;
 
                 string count=to_string(j-i);
                 res.append(count);
-                res.push_back(s[i]);
+                res.push_back(prev[i]);
                 i=j-1;
             }
 
-            cur=res;
-            prev=cur;
+            prev=res;
         }
 
         return prev;
