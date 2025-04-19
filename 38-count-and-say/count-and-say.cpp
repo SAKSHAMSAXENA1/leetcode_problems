1d1b4009a54926750a9ce02aac8ebb8a86df089b
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-19 19:15:56 +0530

    Time: 4 ms (56.34%) | Memory: 10.9 MB (26.12%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-string f(int i,unordered_map<int,string> &dp)
-{
-    if(i==1) return "1"; // base case
-
-    if(dp.find(i)!=dp.end()) return dp[i];
-
-    string s=f(i-1,dp);
-    string res=""; // res is rle
-    int m=s.size();
-
-    for(int i=0;i<m;i++)
-    {
-        int j=i+1;
-        while(j<m && s[i]==s[j])
-        j++;
-
-        string count=to_string(j-i);
-        res.append(count);
-        res.push_back(s[i]);
-        i=j-1;
-    }
-
-    return dp[i]=res;
-}
     string countAndSay(int n) {
         unordered_map<int,string> dp;
-        /*dp[1]="1"; // base case
+        dp[1]="1"; // base case
 
-        for(int i=2;i<=n;i++)
+        for(int k=2;k<=n;k++)
         {
-            
-        }*/
-        return f(n,dp);
+            string s=dp[k-1];
+            string res=""; // res is rle
+            int m=s.size();
+
+            for(int i=0;i<m;i++)
+            {
+                int j=i+1;
+                while(j<m && s[i]==s[j])
+                j++;
+
+                string count=to_string(j-i);
+                res.append(count);
+                res.push_back(s[i]);
+                i=j-1;
+            }
+
+            dp[k]=res;
+        }
+
+        return dp[n];
     }
 };
