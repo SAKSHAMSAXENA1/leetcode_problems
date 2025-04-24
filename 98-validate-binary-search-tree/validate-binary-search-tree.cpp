ad0695463b28e323c92043897784964cb51b43a9
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-24 19:04:33 +0530

    Time: 0 ms (100.00%) | Memory: 21.9 MB (47.85%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int minVal(TreeNode* root)
+{
+    if(root==NULL) return INT_MAX;
+    return min(root->val,min(minVal(root->left),minVal(root->right)));
+}
+
+int maxVal(TreeNode* root)
+{
+    if(root==NULL) return INT_MIN;
+    return max(root->val,max(maxVal(root->left),maxVal(root->right)));
+}
+    bool isValidBST(TreeNode* root) {
+        if(root==NULL) return true;
+
+        bool leftSmaller=(root->left) ? maxVal(root->left) < root->val : true;
+        bool rightBigger=(root->right) ? root->val < minVal(root->right) : true;
+
+        return leftSmaller && rightBigger && isValidBST(root->left) && isValidBST(root->right);
+    }
+};
