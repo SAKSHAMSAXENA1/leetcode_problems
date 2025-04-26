10c7d5e664ae173a10274a448b99374acb00a820
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-26 19:40:01 +0530

    Time: 55 ms (59.69%) | Memory: 122.9 MB (52.88%) - LeetSync

--- 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
+++ 2915-count-of-interesting-subarrays/count-of-interesting-subarrays.cpp
@@ -4,30 +4,24 @@ public:
     long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
         // variant of subarray sum divisible by k 
         unordered_map<int,int> remainderCnt;
-        vector<int> prefixCnt;
         remainderCnt[0]=1;
         int cnt=0; // cumulative/floating count of elements 
         ll res=0;
 
         for(auto num:nums)
         {
-            int remainder=num%modulo;
-            cnt+=(remainder==k);
-            prefixCnt.push_back(cnt);
+            cnt+=((num%modulo)==k);
 
-            /*if(k==0) // for k=0, this probm is same as subarray sum divisible by k
-            res+=remainderCnt*/
-        }
-
-        for(auto it: prefixCnt)
-        {
-            int remainder=it%modulo;
+            int remainder=cnt%modulo;
             if(remainder-k<0)
                 res+=remainderCnt[modulo-abs(remainder-k)%modulo];
             else
                 res+=remainderCnt[remainder-k];
 
             remainderCnt[remainder]++;
+
+            /*if(k==0) // for k=0, this probm is same as subarray sum divisible by k
+            res+=remainderCnt*/
         }
 
         return res;
