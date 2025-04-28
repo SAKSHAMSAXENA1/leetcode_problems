e424a94d1fccc23b80d6e21ab3f47ca3d05f84f3
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-28 14:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (99.33%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -13,22 +13,32 @@ class Solution {
 public:
 void insertNodeIntoBST(TreeNode* root,int &key)
 {
-    if(key < root->val) // key must on left side of root
+    while(root)
     {
-        if(root->left) // search key on left side of root
-            insertNodeIntoBST(root->left,key);
-        else
-            root->left=new TreeNode(key); // insert on left side of root
-    }
-    else // root->val < key -> key must be on right side of root
-    {
-        if(root->right) // search key on right side of root
-            insertNodeIntoBST(root->right,key);
-        else
-            root->right=new TreeNode(key); // insert key on right side of root
+        if(key < root->val) // key must on left side of root
+        {
+            if(root->left) // search key on left side of root
+                root=root->left;
+            else
+            {
+                root->left=new TreeNode(key); // insert on left side of root
+                break;
+            }
+        }
+        else // root->val < key -> key must be on right side of root
+        {
+            if(root->right) // search key on right side of root
+                root=root->right;
+            else
+            {
+                root->right=new TreeNode(key); // insert key on right side of root
+                break;
+            }
+        }
     }
 }
     TreeNode* bstFromPreorder(vector<int>& preorder) {
+        // each node is inserted as a leaf node into the already existing BST
         TreeNode* root=new TreeNode(preorder[0]);
         int n=preorder.size();
 
