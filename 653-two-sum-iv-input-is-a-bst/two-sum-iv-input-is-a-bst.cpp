57132af54bcd573ebd6812d4ee982cffadf1d163
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-04-29 12:36:21 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (12.58%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -13,14 +13,15 @@ class Solution {
 public:
 unordered_set<int> u;
     bool findTarget(TreeNode* root, int k) {
+        // inorder traversal of BST is sorted in ascending order
         if(root==NULL) return false;
 
-        if(findTarget(root->left,k)==true) return true;
+        if(findTarget(root->left,k)==true) return true; // check for left subtree
 
         int rem=k-root->val;
-        if(u.find(rem)!=u.end()) return true;
+        if(u.find(rem)!=u.end()) return true; // check for root
         u.insert(root->val);
 
-        return findTarget(root->right,k);
+        return findTarget(root->right,k); // check for right subtree
     }
 };
