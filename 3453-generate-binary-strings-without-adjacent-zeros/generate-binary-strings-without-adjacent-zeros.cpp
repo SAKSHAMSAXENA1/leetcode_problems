699701cffbeaa11ebe93dee1787f705ef7ff7c23
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-05-14 10:30:55 +0530

    Time: 24 ms (24.20%) | Memory: 33.7 MB (18.89%) - LeetSync

---
+++ 3453-generate-binary-strings-without-adjacent-zeros/generate-binary-strings-without-adjacent-zeros.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+void f(int index,string ds,char lastBit,int &n,vector<string> &res)
+{
+    if(index==n)
+    {
+        res.push_back(ds);
+        return;
+    }
+
+    ds.push_back('1'); // you can always put a '1'
+    f(index+1,ds,'1',n,res);
+    ds.pop_back();
+
+    if(lastBit=='1') // but you can place a '0' only if lastBit was '1'
+    {
+        ds.push_back('0');
+        f(index+1,ds,'0',n,res);
+    }
+}
+    vector<string> validStrings(int n) {
+        vector<string> res;
+        f(0,"",'1',n,res);
+        return res;
+    }
+};
