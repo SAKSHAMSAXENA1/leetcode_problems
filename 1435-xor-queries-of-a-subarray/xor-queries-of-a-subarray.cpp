e70b13f95d0446ccd1c6786235939396886ab701
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-05-15 14:18:35 +0530

    Time: 35 ms (17.87%) | Memory: 100.5 MB (19.86%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -1,52 +1,18 @@
-class SGTree{
-    unordered_map<int,int> seg;
-    public:
-    void build(int index,int low,int high,vector<int> &arr)
-    {
-        if(low==high)
-        {
-            seg[index]=arr[low];
-            return;
-        }
-
-        int mid=(low+high)/2;
-        build(2*index+1,low,mid,arr); // left
-        build(2*index+2,mid+1,high,arr); // right
-        seg[index]=seg[2*index+1] ^ seg[2*index+2];
-    }
-
-    int query(int index,int low,int high,int l,int r)
-    {
-        // no overlap
-        if(high<l || r<low) return 0;
-
-        // complete overlap
-        if(l<=low && high<=r) return seg[index];
-
-        // else partial overlap=no overlap(left)+complete overlap(right) or vice-versa
-        int mid=(low+high)/2;
-        int left=query(2*index+1,low,mid,l,r);
-        int right=query(2*index+2,mid+1,high,l,r);
-        return left ^ right;
-    }
-
-    SGTree(vector<int> &arr)
-    {
-        int n=arr.size();
-        build(0,0,n-1,arr);
-    }
-};
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        SGTree sgt(arr);
         int n=arr.size();
-        vector<int> res;
+        vector<int> prefix(n),res;
+        prefix[0]=arr[0];
 
-        for(auto query:queries)
+        for(int i=1;i<n;i++)
+        prefix[i]=arr[i] ^ prefix[i-1];
+
+        for(auto it:queries)
         {
-            int l=query[0],r=query[1];
-            res.push_back(sgt.query(0,0,n-1,l,r));
+            int l=it[0],r=it[1];
+            int val=prefix[r] ^ ((l>0) ? prefix[l-1] : 0);
+            res.push_back(val);
         }
 
         return res;
