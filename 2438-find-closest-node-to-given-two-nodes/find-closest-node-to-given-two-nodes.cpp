63a65c09c8a28fe68341deb7d0205091f34e8dce
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-05-31 17:02:01 +0530

    Time: 0 ms (100.00%) | Memory: 101.7 MB (97.91%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-void dfs(int node,vector<int> &edges,vector<bool> &vis,vector<int> &dist)
+void dfs(int node,vector<int> &edges,vector<int> &dist)
 {
-    vis[node]=true;
-
     int adjNode=edges[node];
 
-    if(adjNode!=-1 && !vis[adjNode])
+    if(adjNode!=-1 && dist[node]+1<dist[adjNode])
     {
         dist[adjNode]=dist[node]+1;
-        dfs(adjNode,edges,vis,dist);
+        dfs(adjNode,edges,dist);
     }
 }
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n=edges.size(),minDist=INT_MAX,minDistNode=-1;
         vector<int> distFromNode1(n,INT_MAX),distFromNode2(n,INT_MAX);
-        vector<bool> vis1(n,false),vis2(n,false);
 
         distFromNode1[node1]=0;
         distFromNode2[node2]=0;
 
-        dfs(node1,edges,vis1,distFromNode1);
-        dfs(node2,edges,vis2,distFromNode2);
+        dfs(node1,edges,distFromNode1);
+        dfs(node2,edges,distFromNode2);
 
         for(int i=0;i<n;i++)
         {
