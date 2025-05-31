77cc25ef62f51fe54e0ff25256b1804f6c7397b5
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-05-31 17:00:56 +0530

    Time: 27 ms (37.94%) | Memory: 102.2 MB (75.88%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,70 +1,39 @@
 class Solution {
 public:
-    pair<int,int> f(vector<int>& edges, int node1, int node2) {
-        int n=edges.size();
-        vector<int> distFromNode1(n,INT_MAX);
-        vector<bool> vis(n,false);
-        queue<int> q;
-        q.push(node1);
-        vis[node1]=true;
-        distFromNode1[node1]=0; // node1->node1 dist is 0;
+void dfs(int node,vector<int> &edges,vector<bool> &vis,vector<int> &dist)
+{
+    vis[node]=true;
 
-        while(!q.empty())
-        {
-            int curNode=q.front();
-            q.pop();
+    int adjNode=edges[node];
 
-            int adjNode=edges[curNode];
-
-            if(adjNode!=-1 && distFromNode1[curNode]+1<distFromNode1[adjNode])
-            {
-                distFromNode1[adjNode]=distFromNode1[curNode]+1;
-                q.push(adjNode);
-                vis[adjNode]=true;
-            }
-        }
-        
-        if(vis[node2]) //return distFromNode1[node2];
-        return {distFromNode1[node2],node2};
-
-        q.push(node2);
-        int distFromNode2=0;
-        vis[node2]=true;
-
-        while(!q.empty())
-        {
-            int curNode=q.front();
-            q.pop();
-
-            int adjNode=edges[curNode];
-
-            if(adjNode!=-1)
-            {
-                if(vis[adjNode]) 
-                return {max(distFromNode1[adjNode],distFromNode2+1),adjNode};
-                else
-                {
-                    q.push(adjNode);
-                    vis[adjNode]=true;
-                    distFromNode2++;
-                }
-            }
-
-        }
-
-        return {INT_MAX,-1};
+    if(adjNode!=-1 && !vis[adjNode])
+    {
+        dist[adjNode]=dist[node]+1;
+        dfs(adjNode,edges,vis,dist);
     }
-
+}
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        auto [dist1,commonNode1]=f(edges,node1,node2);
-        auto [dist2,commonNode2]=f(edges,node2,node1);
+        int n=edges.size(),minDist=INT_MAX,minDistNode=-1;
+        vector<int> distFromNode1(n,INT_MAX),distFromNode2(n,INT_MAX);
+        vector<bool> vis1(n,false),vis2(n,false);
 
-        if(dist1<dist2) return commonNode1;
-        else if(dist2<dist1) return commonNode2;
+        distFromNode1[node1]=0;
+        distFromNode2[node2]=0;
 
-        // else dist1==dist2
-        if(dist1==INT_MAX) return -1;
+        dfs(node1,edges,vis1,distFromNode1);
+        dfs(node2,edges,vis2,distFromNode2);
 
-        return min(commonNode1,commonNode2);
+        for(int i=0;i<n;i++)
+        {
+            int distOfCurNode=max(distFromNode1[i],distFromNode2[i]);
+
+            if(distOfCurNode < minDist)
+            {
+                minDist=distOfCurNode;
+                minDistNode=i;
+            }
+        }
+
+        return minDistNode;
     }
 };
