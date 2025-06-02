df99cbf2c9a4180e798d2ca6090013f90f2ce188
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-06-03 01:51:41 +0530

    Time: 0 ms (100.00%) | Memory: 22.9 MB (43.53%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -2,22 +2,22 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int sum=0,n=ratings.size();
-        vector<int> L2R(n,1),R2L(n,1);
+        vector<int> cnt(n,1);
 
         for(int i=1;i<n;i++)
         {
             if(ratings[i-1] < ratings[i])
-            L2R[i]=L2R[i-1]+1;
+            cnt[i]=cnt[i-1]+1;
         }
 
         for(int i=n-2;i>=0;i--)
         {
             if(ratings[i] > ratings[i+1])
-            R2L[i]=R2L[i+1]+1;
+            cnt[i]=max(cnt[i],cnt[i+1]+1);
         }
 
-        for(int i=0;i<n;i++)
-        sum+=max(L2R[i],R2L[i]);
+        for(auto it:cnt)
+        sum+=it;
 
         return sum;
     }
