63af2335f4142dadffa26a6a8eaad9f4409b41ae
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-06-05 15:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (96.66%) - LeetSync

---
+++ 1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.cpp
@@ -0,0 +1,52 @@
+class DisjointSet{
+    int parent[26]={0};
+public:
+    DisjointSet()
+    {
+        for(int i=0;i<26;i++)
+        parent[i]=i; // each char is a parent of itself
+    }
+
+    int findUPar(int node)
+    {
+        if(node==parent[node]) return node;
+        else return parent[node]=findUPar(parent[node]);
+    }
+
+    void unionByLexicographicalOrder(int u,int v)
+    {
+        int ulp_u=findUPar(u);
+        int ulp_v=findUPar(v);
+
+        if(ulp_u == ulp_v) // both u & v already part of the same component
+        return;
+
+        if(ulp_u < ulp_v)
+        {
+            parent[ulp_v]=ulp_u;
+        }
+        else // ulp_v < ulp_u
+        {
+            parent[ulp_u]=ulp_v;
+        }
+    }
+};
+class Solution {
+public:
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        DisjointSet ds;
+        int n=s1.size();
+
+        for(int i=0;i<n;i++)
+        {
+            ds.unionByLexicographicalOrder(s1[i]-'a',s2[i]-'a');
+        }
+
+        for(auto &it:baseStr)
+        {
+            it=ds.findUPar(it-'a')+'a';
+        }
+
+        return baseStr;
+    }
+};
