35f632bdcfd1b6e809c18a9608deb35285da0553
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-06-09 00:44:10 +0530

    Time: 16 ms (44.40%) | Memory: 163.2 MB (38.60%) - LeetSync

---
+++ 3876-transform-array-to-all-equal-elements/transform-array-to-all-equal-elements.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+bool canMake(vector<int> temp,int &k,int target,int &n)
+{
+    int operationsCnt=0;
+    for(int i=0;i<n-1;i++)
+    {
+        if(temp[i]!=target)
+        {
+            temp[i]*=-1;
+            temp[i+1]*=-1;
+            operationsCnt++;
+        }
+    }
+
+    return operationsCnt<=k && temp[n-1]==target;
+}
+    bool canMakeEqual(vector<int>& nums, int k) {
+        int n=nums.size();
+
+        if(canMake(nums,k,1,n)) return true;
+
+        return canMake(nums,k,-1,n);
+    }
+};
