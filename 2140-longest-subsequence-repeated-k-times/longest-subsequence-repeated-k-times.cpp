b0cd40dfa2e8dea0aadd3bc8d312a5d204b5050e
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-06-27 23:17:41 +0530

    Time: 265 ms (59.42%) | Memory: 10.5 MB (99.28%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -22,8 +22,8 @@ bool isRepeatedKTimes(string &ds,string &s,int &k)
     return cnt>=k;
 }
 
-bool canGenerateSubsequences(string &ds,vector<bool> &canUse,
-vector<int> &remCharFreqPerSubsequence, int &maxLen,string &res,string &s,int &k)
+bool canGenerateSubsequences(string &ds, vector<int> &remCharFreqPerSubsequence, int &maxLen,
+string &res,string &s,int &k)
 {
     if(ds.size()==maxLen)
     {
@@ -36,12 +36,12 @@ vector<int> &remCharFreqPerSubsequence, int &maxLen,string &res,string &s,int &k
 
     for(int i=25;i>=0;i--)
     {
-        if(canUse[i]==false || remCharFreqPerSubsequence[i]==0) continue;
+        if(remCharFreqPerSubsequence[i]==0) continue;
 
         ds.push_back(i+'a');
         remCharFreqPerSubsequence[i]--;
 
-        if(canGenerateSubsequences(ds,canUse,remCharFreqPerSubsequence,maxLen,res,s,k))
+        if(canGenerateSubsequences(ds,remCharFreqPerSubsequence,maxLen,res,s,k))
         return true;
 
         // backtrack
@@ -58,16 +58,11 @@ vector<int> &remCharFreqPerSubsequence, int &maxLen,string &res,string &s,int &k
         for(auto it:s)
         freq[it-'a']++;
 
-        vector<bool> canUse(26,false);
         vector<int> charFreqPerSubsq(26,0);
 
         for(int i=0;i<26;i++)
         {
-            if(freq[i]>=k)
-            {
-                canUse[i]=true;
-                charFreqPerSubsq[i]=freq[i]/k;
-            }
+            charFreqPerSubsq[i]=freq[i]/k;
         }
 
         string res="";
@@ -75,7 +70,7 @@ vector<int> &remCharFreqPerSubsequence, int &maxLen,string &res,string &s,int &k
         for(int subsequenceLen=maxLen;subsequenceLen>=0;subsequenceLen--)
         {
             string cur="";
-            if(canGenerateSubsequences(cur,canUse,charFreqPerSubsq,subsequenceLen,res,s,k))
+            if(canGenerateSubsequences(cur,charFreqPerSubsq,subsequenceLen,res,s,k))
             break;
         }
         
