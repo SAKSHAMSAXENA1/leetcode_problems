4c00106358f84cf8d0c4ab6df7072fb4fcc8d6a3
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-06-27 23:15:56 +0530

    Time: 285 ms (56.52%) | Memory: 10.8 MB (94.20%) - LeetSync

--- 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.cpp
@@ -22,30 +22,34 @@ bool isRepeatedKTimes(string &ds,string &s,int &k)
     return cnt>=k;
 }
 
-void generateSubsequences(string &ds,vector<bool> &canUse,vector<int> &remCharFreqPerSubsequence,
-int &maxLen,string &res,string &s,int &k)
+bool canGenerateSubsequences(string &ds,vector<bool> &canUse,
+vector<int> &remCharFreqPerSubsequence, int &maxLen,string &res,string &s,int &k)
 {
-    if(ds.size()>maxLen) return;
-
-    if(ds.size()>res.size() || (ds.size()==res.size() && ds>res))
+    if(ds.size()==maxLen)
     {
         if(isRepeatedKTimes(ds,s,k))
-        res=ds;
+        {
+            res=ds;
+            return true;
+        }
     }
 
-    for(int i=0;i<26;i++)
+    for(int i=25;i>=0;i--)
     {
         if(canUse[i]==false || remCharFreqPerSubsequence[i]==0) continue;
 
         ds.push_back(i+'a');
         remCharFreqPerSubsequence[i]--;
 
-        generateSubsequences(ds,canUse,remCharFreqPerSubsequence,maxLen,res,s,k);
+        if(canGenerateSubsequences(ds,canUse,remCharFreqPerSubsequence,maxLen,res,s,k))
+        return true;
 
         // backtrack
         ds.pop_back();
         remCharFreqPerSubsequence[i]++;
     }
+
+    return false;
 }
     string longestSubsequenceRepeatedK(string s, int k) {
         int n=s.size(),maxLen=n/k;
@@ -66,10 +70,15 @@ int &maxLen,string &res,string &s,int &k)
             }
         }
 
-        string res="",subsequence="";
+        string res="";
 
-        generateSubsequences(subsequence,canUse,charFreqPerSubsq,maxLen,res,s,k);
-
+        for(int subsequenceLen=maxLen;subsequenceLen>=0;subsequenceLen--)
+        {
+            string cur="";
+            if(canGenerateSubsequences(cur,canUse,charFreqPerSubsq,subsequenceLen,res,s,k))
+            break;
+        }
+        
         return res;
     }
 };
