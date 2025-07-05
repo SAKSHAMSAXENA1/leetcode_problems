748f0973323359f5baaaf7c674715a840acab8d7
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-05 16:44:29 +0530

    Time: 19 ms (32.16%) | Memory: 11.1 MB (75.55%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -19,7 +19,25 @@ int f(int index,int rem,int &n,int &k,vector<vector<int>> &dp)
     return dp[index][rem]=noOfWays;
 }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        return f(0,target,n,k,dp);
+        vector<vector<int>> dp(n+1,vector<int>(target+1,0));
+        dp[n][0]=1;
+
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int rem=0;rem<=target;rem++)
+            {
+                //if(n-index>rem) break;
+
+                int noOfWays=0;
+
+                for(int faceVal=1;faceVal<=k && faceVal<=rem;faceVal++)
+                {
+                    noOfWays=(noOfWays+dp[index+1][rem-faceVal])%mod;
+                }
+
+                dp[index][rem]=noOfWays;
+            }
+        }
+        return dp[0][target];
     }
 };
