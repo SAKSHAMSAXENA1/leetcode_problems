53f9535edc3707f5796c3d7118f013973790e2d4
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-05 17:24:10 +0530

    Time: 0 ms (100.00%) | Memory: 59.2 MB (91.14%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // longest window/subarray containing at most k 0's
-        int l=0,r=0,n=nums.size(),maxLen=0;
-        int cntOfZeros=0;
+        int res=0,r=0,l=0,n=nums.size(),ones=0;
 
         while(r<n)
         {
-            cntOfZeros+=(nums[r]==0); // (acquire)
+            ones+=nums[r]; // acquire
 
-            if(cntOfZeros>k)
+            // while the condn of probm: no of flips<=k is false
+            while(r-l+1 - ones > k) // flips=tot elements - ones
             {
-                cntOfZeros-=(nums[l]==0); // (release)
+                ones-=nums[l]; // release;
                 l++; // shrink the window (release)
             }
 
-            // update maxLen only when the window has <=k 0's
-            if(cntOfZeros<=k)
-            maxLen=max(maxLen,r-l+1);
-
+            // now condn of probm: flips<=k is true, so update the answer
+            res=max(res,r-l+1);
             r++; // expand the window (acquire)
         }
 
-        return maxLen;
+        return res;
     }
 };
