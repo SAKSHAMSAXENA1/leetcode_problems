d40ec56f8f4c4a068d0f1a8132b98dffb4930043
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-12 02:36:07 +0530

    Time: 343 ms (5.82%) | Memory: 159.9 MB (5.14%) - LeetSync

--- 1860-find-kth-largest-xor-coordinate-value/find-kth-largest-xor-coordinate-value.cpp
+++ 1860-find-kth-largest-xor-coordinate-value/find-kth-largest-xor-coordinate-value.cpp
@@ -4,30 +4,27 @@ public:
         int m=matrix.size(),n=matrix[0].size();
         vector<vector<int>> rowXor(m,vector<int>(n)),colXor(m,vector<int>(n)),
         gridXor(m,vector<int>(n));
+        gridXor[0][0]=matrix[0][0];
         vector<int> xorVal;
-
-        for(int i=0;i<m;i++)
-        {
-            rowXor[i][0]=matrix[i][0];
-            for(int j=1;j<n;j++)
-            rowXor[i][j]=rowXor[i][j-1]^matrix[i][j];
-        }
-
-        for(int j=0;j<n;j++)
-        {
-            colXor[0][j]=matrix[0][j];
-            for(int i=1;i<m;i++)
-            colXor[i][j]=colXor[i-1][j]^matrix[i][j];
-        }
 
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
+                if(i==0) colXor[i][j]=matrix[i][j];
+                if(j==0) rowXor[i][j]=matrix[i][j];
                 int x=matrix[i][j];
                 if(i>=1 && j>=1) x=x^gridXor[i-1][j-1];
-                if(i>=1) x=x^colXor[i-1][j];
-                if(j>=1) x=x^rowXor[i][j-1];
+                if(i>=1)
+                {
+                    x=x^colXor[i-1][j];
+                    colXor[i][j]=colXor[i-1][j]^matrix[i][j];
+                }
+                if(j>=1) 
+                {
+                    x=x^rowXor[i][j-1];
+                    rowXor[i][j]=rowXor[i][j-1]^matrix[i][j];
+                }
                 gridXor[i][j]=x;
                 xorVal.push_back(x);
             }
