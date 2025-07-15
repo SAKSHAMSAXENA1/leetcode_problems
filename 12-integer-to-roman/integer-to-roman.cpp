b312289df0d02cd935489bc6f3da01daa8917c69
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-16 01:41:09 +0530

    Time: 3 ms (62.77%) | Memory: 9.2 MB (84.66%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        //unordered_map<int,string> u;
-        //u[1]="I";u[5]="V";u[10]="X";u[50]="L";u[100]="C";u[500]="D";u[1000]="M";
         string res="";
 
-        int placeVal=1;
+        float placeVal=0.1;
 
         while(num)
         {
             int dig=num%10;
-            //int val=dig*placeVal;
             bool digGTE5=false;
+            placeVal*=10;
             num/=10;
 
             if(dig==4)
             {
-                if(placeVal==1) res="IV"+res;
-                else if(placeVal==10) res="XL"+res;
-                else if(placeVal==100) res="CD"+res;
-
-                placeVal*=10;
+                if(placeVal==1) res="IV"+res; // 4
+                else if(placeVal==10) res="XL"+res; // 40
+                else if(placeVal==100) res="CD"+res; // 400
                 continue;
             }
             else if(dig==9)
             {
-                if(placeVal==1) res="IX"+res;
-                else if(placeVal==10) res="XC"+res;
-                else if(placeVal==100) res="CM"+res;
-
-                placeVal*=10;
+                if(placeVal==1) res="IX"+res; // 9
+                else if(placeVal==10) res="XC"+res; // 90
+                else if(placeVal==100) res="CM"+res; // 900
                 continue;
             }
             else if(dig>=5)
@@ -40,20 +34,18 @@ public:
 
             while(dig--)
             {
-                if(placeVal==1) res="I"+res;
-                else if(placeVal==10) res="X"+res;
-                else if(placeVal==100) res="C"+res;
-                else res="M"+res;
+                if(placeVal==1) res="I"+res; // 1
+                else if(placeVal==10) res="X"+res; // 10
+                else if(placeVal==100) res="C"+res; // 100
+                else res="M"+res; // 1000
             }
 
             if(digGTE5)
             {
-                if(placeVal==1) res="V"+res;
-                else if(placeVal==10) res="L"+res;
-                else if(placeVal==100) res="D"+res;
+                if(placeVal==1) res="V"+res; // 5
+                else if(placeVal==10) res="L"+res; // 50
+                else if(placeVal==100) res="D"+res; // 500
             }
-
-            placeVal*=10;
         }
 
         return res;
