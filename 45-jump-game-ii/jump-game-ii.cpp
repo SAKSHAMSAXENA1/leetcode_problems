d115814d9dd3b6b8127e1aae60cb5adf27e58989
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-21 17:34:18 +0530

    Time: 69 ms (19.07%) | Memory: 21.6 MB (17.41%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/* // Memoization
 int f(int index,vector<int> &nums,vector<int> &dp,int &n)
 {
     if(index==n-1) return 0;
@@ -18,9 +19,25 @@ int f(int index,vector<int> &nums,vector<int> &dp,int &n)
 
     return dp[index]=mini;
 }
+*/
     int jump(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n,-1);
-        return f(0,nums,dp,n);
+        vector<int> dp(n,0);
+
+        for(int index=n-2;index>=0;index--)
+        {
+            int mini=INT_MAX;
+
+            for(int jumps=1;jumps<=nums[index] && index+jumps<n;jumps++)
+            {
+                int nextJumps=dp[index+jumps];
+
+                if(nextJumps!=INT_MAX)
+                mini=min(mini,1+nextJumps);
+            }
+
+            dp[index]=mini;
+        }
+        return dp[0];
     }
 };
