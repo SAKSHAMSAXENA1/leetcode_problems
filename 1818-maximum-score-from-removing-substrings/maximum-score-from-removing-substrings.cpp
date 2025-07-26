69a7c0953908e1a015b9f6d6db7c1b45dfd8b22b
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-26 18:11:19 +0530

    Time: 26 ms (74.07%) | Memory: 19.7 MB (88.91%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.cpp
@@ -2,19 +2,27 @@ class Solution {
 public:
 int findPoints(string &s,string &str,int &x)
 {
-    string temp="";
-    int points=0;
-    for(auto it:s)
+    int i=0; // writing pointer, all char before 'i' represent my ans after pattern 'str'
+    // has been removed from 's'
+    // hence char from 'i' onwards are the ones that are to be deleted to get the 'ans'
+    // after performing equivalent stack operations
+
+    int n=s.size(),points=0;
+    for(int j=0;j<n;j++) // j is the reading pointer
     {
-        if(it==str[1] && !temp.empty() && temp.back()==str[0])
+        s[i]=s[j]; // char read by 'j' is written at 'i' in 's'
+        i++;
+
+        if(i>=2 && s[i-2]==str[0] && s[i-1]==str[1]) // pattern str is found in s
         {
-            temp.pop_back();
             points+=x;
+            i-=2;
+            // char s[i-2] && s[i-1] are to be deleted, hence overwriting them will not 
+            // make any difference (safe to overwrite them)
         }
-        else temp.push_back(it);
     }
 
-    s=temp;
+    s.erase(s.begin()+i,s.end());
     return points;
 }
     int maximumGain(string s, int x, int y) {
