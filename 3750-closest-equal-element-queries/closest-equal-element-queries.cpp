e6a6992086618622edbcdf472eadc996cc01e4f4
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-27 12:30:30 +0530

    Time: 427 ms (18.94%) | Memory: 219.1 MB (33.42%) - LeetSync

---
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.cpp
@@ -0,0 +1,90 @@
+class Solution {
+public:
+int binarySearch(vector<int> &nums,int &key,int &n)
+{
+    int l=0,h=n-1;
+
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+
+        if(nums[mid]==key) return mid;
+        else if(key<nums[mid]) // search on left
+        h=mid-1;
+        else // nums[mid]<key => search on right
+        l=mid+1;
+    }
+
+    return -1;
+}
+    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+        map<int,vector<int>> u;
+        unordered_map<int,int> shortestDist;
+        vector<int> res;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
+        u[nums[i]].push_back(i);
+
+        /*for(auto it:u)
+        {
+            cout<<endl<<it.first<<" : ";
+            for(auto index:it.second)
+            cout<<index<<" ";
+        }*/
+
+        /*for(auto &[el,indices]:u)
+        {
+            int m=indices.size();
+            if(m==1) shortestDist[el]=-1;
+            else
+            {
+                sort(indices.begin(),indices.end());
+                int mini=INT_MAX;
+                for(int i=0;i<m-1;i++)
+                mini=min({mini,indices[i+1]-indices[i],indices[i]+n-indices[i+1]});
+
+                mini=min({mini,indices[m-1]-indices[0],indices[0]+n-indices[m-1]});
+                
+                shortestDist[el]=mini;
+            } 
+        }*/
+
+        for(auto index: queries)
+        {
+            int element=nums[index];
+            int m=u[element].size();
+            if(m==1)
+            {
+                res.push_back(-1);
+                continue;
+            }
+
+            int ind=binarySearch(u[element],index,m);
+            int prev,next,mini;
+
+            if(ind==0)
+            {
+                next=min(u[element][ind+1]-index,index+n-u[element][ind+1]); // next at ind+1
+                prev=min(u[element][m-1]-ind,index+n-u[element][m-1]); // prev at m-1
+                mini=min(prev,next);
+            }
+            else if(ind==m-1)
+            {
+                prev=min(index-u[element][ind-1],u[element][ind-1]+n-index); // prev at ind-1
+                next=min(index-u[element][0],u[element][0]+n-index); // next at 0
+                mini=min(prev,next);
+            }
+            else
+            {
+                prev=min(index-u[element][ind-1],u[element][ind-1]+n-index); // prev at ind-1
+                next=min(u[element][ind+1]-index,index+n-u[element][ind+1]); // next at ind+1
+                mini=min(prev,next);
+            }
+
+            res.push_back(mini);
+        }
+
+        return res;
+    }
+};
