0317a2e321bc30bb15ce560aad0b777ec1d997fd
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-07-28 18:44:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (75.19%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-bool dfs(int node,vector<vector<int>> &graph, vector<int> &color)
+bool isComponentBipartite(int node,vector<vector<int>> &graphs,vector<int> &color)
 {
-    for(auto neighbour:graph[node])
+    for(auto neighbour:graphs[node])
     {
-        if(color[neighbour]==-1)
+        if(color[neighbour]==-1) // neighbour is not yet colored
         {
             color[neighbour]=!color[node];
-            if(dfs(neighbour,graph,color)==false) return false;
+            if(isComponentBipartite(neighbour,graphs,color)==false)
+            return false;
         }
         else if(color[neighbour]==color[node]) return false;
     }
 
+    // no false was returned
     return true;
 }
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int> color(n,-1);
+        vector<int> color(n,-1); // initially all node uncolored
 
-        for(int i=0;i<n;i++)
+        for(int node=0;node<n;node++)
         {
-            if(color[i]==-1)
+            if(color[node]==-1) // node not colored yet
             {
-                color[i]=0;
-                if(dfs(i,graph,color)==false) return false;
+                color[node]=0;
+                if(isComponentBipartite(node,graph,color)==false)
+                return false;
             }
         }
 
+        // all components are bipartite
         return true;
     }
 };
