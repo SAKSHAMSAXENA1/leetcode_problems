c13f7568198d4eb46adf033e48f0f5d78032ac92
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-07 23:16:01 +0530

    Time: 480 ms (5.19%) | Memory: 223.1 MB (7.14%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -2,32 +2,42 @@ class Solution {
 public:
 int f(int startI,int startJ,vector<vector<int>> &fruits,int &n,int factor[],int di[],int dj[])
 {
-    queue<pair<int,pair<int,int>>> q;
+    priority_queue<pair<int,pair<int,int>>> pq;
     vector<vector<int>> dist(n,vector<int>(n,INT_MIN));
-    q.push({0,{startI,startJ}});
+    pq.push({0,{startI,startJ}});
     
-    while(!q.empty())
+    while(!pq.empty())
     {
-        int curDist=q.front().first,i=q.front().second.first,j=q.front().second.second;
-        q.pop();
+        int curLevelSize=pq.size();
+        stack<pair<int,pair<int,int>>> temp;
 
-        if(curDist<dist[i][j]) continue;
-
-        if(i==n-1 && j==n-1) return dist[i][j];
-
-        for(int k=0;k<3;k++)
+        while(curLevelSize--)
         {
-            int ni=i+di[k],nj=j+dj[k];
-            int remMoves=(n-1-ni)*factor[0]+(n-1-nj)*factor[1];
+            int curDist=pq.top().first,i=pq.top().second.first,j=pq.top().second.second;
+            pq.pop();
 
-            if(0<=ni && ni<n && 0<=nj && nj<n && ((n-1-ni)+(n-1-nj))<=2*remMoves)
+            if(i==n-1 && j==n-1) return dist[i][j];
+
+            for(int k=0;k<3;k++)
             {
-                if(curDist+fruits[i][j] > dist[ni][nj])
+                int ni=i+di[k],nj=j+dj[k];
+                int remMoves=(n-1-ni)*factor[0]+(n-1-nj)*factor[1];
+
+                if(0<=ni && ni<n && 0<=nj && nj<n && ((n-1-ni)+(n-1-nj))<=2*remMoves)
                 {
-                    dist[ni][nj]=curDist+fruits[i][j];
-                    q.push({dist[ni][nj],{ni,nj}});
+                    if(curDist+fruits[i][j] > dist[ni][nj])
+                    {
+                        dist[ni][nj]=curDist+fruits[i][j];
+                        temp.push({dist[ni][nj],{ni,nj}});
+                    }
                 }
             }
+        }
+
+        while(!temp.empty())
+        {
+            pq.push(temp.top());
+            temp.pop();
         }
     }
 
@@ -45,7 +55,6 @@ int f(int startI,int startJ,vector<vector<int>> &fruits,int &n,int factor[],int 
             fruits[i][i]=0;
         }
 
-        //priority_queue<pair<int,pair<int,int>>> pq;
         return res+f(0,n-1,fruits,n,factor1,di1,dj1)+f(n-1,0,fruits,n,factor2,di2,dj2); 
     }
 };
