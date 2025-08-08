58e257b3ca01e5e45e31209f9e8d061185323e11
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-09 01:34:32 +0530

    Time: 102 ms (12.11%) | Memory: 105.3 MB (13.10%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,55 +1,8 @@
-class SegmentTree{
-public:
-    vector<int> seg;
-
-    void build(int index,int low,int high,vector<vector<int>> &nums)
-    {
-        if(low==high) // node interval converges to a range
-        {
-            seg[index]=nums[low][1];
-            return;
-        }
-
-        int mid=(low+high)/2;
-        build(2*index+1,low,mid,nums);
-        build(2*index+2,mid+1,high,nums);
-
-        seg[index]=max(seg[2*index+1],seg[2*index+2]);
-    }
-
-    SegmentTree(int &n,vector<vector<int>> &nums)
-    {
-        seg.resize(4*n);
-        build(0,0,n-1,nums);
-    }
-
-    int query(int index,int low,int high,int r)
-    {
-        // no overlap: node interval lies outside query range
-        // [0 r][low high]
-        if(r < low)
-        return INT_MIN; // very small val which prevents elements of this interval from inclusion
-
-        // complete overlap: node interval lies completely inside query range
-        // [0 low high r]
-        if(0<=low && high<=r)
-        return seg[index];
-
-        // else partial overlap:no overlap(left)+complete overlap(right)
-        //                   or:complete overlap(left)+no overlap(right)
-        int mid=(low+high)/2;
-        int left=query(2*index+1,low,mid,r);
-        int right=query(2*index+2,mid+1,high,r);
-
-        return max(left,right);
-    }
-
-};
 class Solution {
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
         int n=items.size();
-        vector<int> res,prices;
+        vector<int> res,prices,prefixMax(n);
         for(auto it:items)
         {
             prices.push_back(it[0]);
@@ -58,7 +11,10 @@ public:
         sort(prices.begin(),prices.end());
         sort(items.begin(),items.end());
 
-        SegmentTree sgt(n,items);
+        prefixMax[0]=items[0][1];
+
+        for(int i=1;i<n;i++)
+        prefixMax[i]=max(prefixMax[i-1],items[i][1]);
 
         for(auto it:queries)
         {
@@ -68,7 +24,7 @@ public:
             if(ub!=prices.begin()) // cost of atleast 1 item is lesser than equal to it
             {
                 int idx=ub-prices.begin()-1;
-                queryRes=sgt.query(0,0,n-1,idx);
+                queryRes=prefixMax[idx];
             }
 
             res.push_back(queryRes);
