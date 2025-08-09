957481f87fa26c408ceac19e6ded755a3b0da333
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-09 14:40:36 +0530

    Time: 74 ms (28.03%) | Memory: 107.3 MB (10.78%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
+int upperBound(vector<pair<int,int>> &nums,int &n,int &key)
+{
+    int l=0,h=n-1,res=n;
+
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+
+        if(key < nums[mid].first)
+        {
+            res=mid; // one possible ans is index 'mid'
+            h=mid-1; // reduce h in order to find first element which is > key
+        }
+        else l=mid+1;
+    }
+
+    return res;
+}
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
         int n=items.size();
-        vector<int> res,prices,prefixMax(n);
+        vector<int> res,prefixMax(n);
         vector<pair<int,int>> sortedItems;
         for(auto it:items)
         {
-            prices.push_back(it[0]);
             sortedItems.push_back({it[0],it[1]});
         }
 
-        sort(prices.begin(),prices.end());
         sort(sortedItems.begin(),sortedItems.end());
 
         prefixMax[0]=sortedItems[0].second;
@@ -20,12 +36,12 @@ public:
 
         for(auto it:queries)
         {
-            auto ub=upper_bound(prices.begin(),prices.end(),it);
+            int ub=upperBound(sortedItems,n,it);
             int queryRes=0;
 
-            if(ub!=prices.begin()) // cost of atleast 1 item is lesser than equal to it
+            if(ub!=0) // cost of atleast 1 item is lesser than equal to it
             {
-                int idx=ub-prices.begin()-1;
+                int idx=ub-1;
                 queryRes=prefixMax[idx];
             }
 
