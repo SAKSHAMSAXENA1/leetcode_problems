d68af2eca132f4b143e25575169deb33c24bbb1b
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-12 22:50:13 +0530

    Time: 282 ms (41.61%) | Memory: 123.7 MB (38.12%) - LeetSync

---
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,48 @@
+#define ll long long
+#define mod (ll)(1e9+7)
+class Solution {
+public:
+int findLargestBase(ll &n,int &x)
+{
+    ll l=0,h=n,res=0;
+
+    while(l<=h)
+    {
+        ll mid=(l+h)/2;
+
+        if(pow(mid,x) <= n)
+        {
+            res=mid;
+            l=mid+1;
+        }
+        else h=mid-1;
+    }
+
+    return res;
+}
+
+ll f(int i,ll rem,int &x,ll &largestBase,vector<vector<ll>> &dp)
+{
+    if(rem==0) return 1;
+
+    if(i>largestBase) return 0;
+
+    if(dp[i][rem]!=-1) return dp[i][rem];
+
+    ll cur=pow(i,x);
+
+    ll take=0;
+    if(cur <= rem)
+    take=f(i+1,rem-cur,x,largestBase,dp);
+
+    ll notTake=f(i+1,rem,x,largestBase,dp);
+
+    return dp[i][rem]=(take+notTake)%mod;
+}
+    int numberOfWays(int n, int x) {
+        ll N=n;
+        ll largestBase=findLargestBase(N,x);
+        vector<vector<ll>> dp(largestBase+1,vector<ll>(n+1,-1));
+        return f(1,n,x,largestBase,dp);
+    }
+};
