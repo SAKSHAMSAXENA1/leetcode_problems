18553c0301f8043ff63a0d38c4d3b4da06fc9481
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-17 22:57:30 +0530

    Time: 8 ms (57.58%) | Memory: 97.4 MB (20.45%) - LeetSync

---
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+// Memoization
+bool f(int index,vector<int> &nums,int &n,vector<int> &dp)
+{
+    if(index==n) return true;
+
+    if(dp[index]!=-1) return dp[index];
+
+    bool opt1=false,opt2=false,opt3=false;
+    if(index+1<n && nums[index]==nums[index+1])
+    opt1=f(index+2,nums,n,dp);
+
+    if(index+2<n)
+    {
+        if(nums[index]==nums[index+1] && nums[index+1]==nums[index+2])
+        opt2=f(index+3,nums,n,dp);
+
+        if(nums[index]+1==nums[index+1] && nums[index+1]+1==nums[index+2])
+        opt3=f(index+3,nums,n,dp);
+    }
+
+    return dp[index]=opt1 || opt2 || opt3;
+}
+    bool validPartition(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return f(0,nums,n,dp);
+    }
+};
