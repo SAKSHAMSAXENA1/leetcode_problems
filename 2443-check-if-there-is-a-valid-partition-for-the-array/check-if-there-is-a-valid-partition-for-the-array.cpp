51f576e1e82cfaad27151a3cf49344f6f453f0e0
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-17 23:01:38 +0530

    Time: 7 ms (65.61%) | Memory: 88.3 MB (75.45%) - LeetSync

--- 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/*
 // Memoization
 bool f(int index,vector<int> &nums,int &n,vector<int> &dp)
 {
@@ -26,5 +27,31 @@ bool f(int index,vector<int> &nums,int &n,vector<int> &dp)
         int n=nums.size();
         vector<int> dp(n,-1);
         return f(0,nums,n,dp);
+    }*/
+
+// Tabulation
+    bool validPartition(vector<int>& nums) {
+        int n=nums.size();
+        vector<bool> dp(n+1,true);
+
+        for(int index=n-1;index>=0;index--)
+        {
+            bool opt1=false,opt2=false,opt3=false;
+            if(index+1<n && nums[index]==nums[index+1])
+            opt1=dp[index+2];
+
+            if(index+2<n)
+            {
+                if(nums[index]==nums[index+1] && nums[index+1]==nums[index+2])
+                opt2=dp[index+3];
+
+                if(nums[index]+1==nums[index+1] && nums[index+1]+1==nums[index+2])
+                opt3=dp[index+3];
+            }
+
+            dp[index]=opt1 || opt2 || opt3;
+        }
+        
+        return dp[0];
     }
 };
