382278a5c186c59ce449230d566dc8012ad04ac6
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-22 01:11:06 +0530

    Time: 355 ms (23.76%) | Memory: 123.6 MB (34.73%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,16 +1,16 @@
 #define ll long long
-#define mod (int)(1e9+7)
+#define mod (ll)(1e9+7)
 class Solution {
 public:
-int findLargestBase(int &n,int &x)
+int findLargestNk(int &n,int &x)
 {
-    int l=0,h=n,res=0;
+    int l=1,h=n,res=n;
 
     while(l<=h)
     {
-        ll mid=(l+h)/2;
+        int mid=(l+h)/2;
 
-        if(pow(mid,x) <= (ll)n)
+        if(pow(mid,x) <= n)
         {
             res=mid;
             l=mid+1;
@@ -21,57 +21,25 @@ int findLargestBase(int &n,int &x)
     return res;
 }
 
-/* 
-// Memoition
-ll f(int i,ll rem,int &x,ll &largestBase,vector<vector<ll>> &dp)
+ll f(int i,int n,int &x,int &largestNk,vector<vector<ll>> &dp)
 {
-    if(rem==0) return 1;
+    if(n==0) return 1;
 
-    if(i>largestBase) return 0;
+    if(i>largestNk) return 0;
 
-    if(dp[i][rem]!=-1) return dp[i][rem];
-
-    ll cur=pow(i,x);
+    if(dp[i][n]!=-1) return dp[i][n];
 
     ll take=0;
-    if(cur <= rem)
-    take=f(i+1,rem-cur,x,largestBase,dp);
+    if(pow(i,x) <= n)
+    take=f(i+1,n-pow(i,x),x,largestNk,dp)%mod;
 
-    ll notTake=f(i+1,rem,x,largestBase,dp);
+    ll notTake=f(i+1,n,x,largestNk,dp)%mod;
 
-    return dp[i][rem]=(take+notTake)%mod;
+    return dp[i][n]=(take+notTake)%mod;
 }
     int numberOfWays(int n, int x) {
-        ll N=n;
-        ll largestBase=findLargestBase(N,x);
-        vector<vector<ll>> dp(largestBase+1,vector<ll>(n+1,-1));
-        return f(1,n,x,largestBase,dp);
-    }
-*/
-
-// Tabulation
-    int numberOfWays(int n, int x) {
-        int largestBase=findLargestBase(n,x);
-        vector<vector<int>> dp(largestBase+2,vector<int>(n+1,0));
-
-        for(int i=0;i<=largestBase+1;i++) dp[i][0]=1; // for rem=0, value=1
-
-        for(int i=largestBase;i>=1;i--)
-        {
-            for(int rem=1;rem<=n;rem++)
-            {
-                int cur=pow(i,x);
-
-                int take=0;
-                if(cur <= rem)
-                take=dp[i+1][rem-cur];
-
-                int notTake=dp[i+1][rem];
-
-                dp[i][rem]=((ll)take+notTake)%mod;
-            }
-        } 
-
-        return dp[1][n];
+        int largestNk=findLargestNk(n,x);
+        vector<vector<ll>> dp(largestNk+1,vector<ll>(n+1,-1));
+        return f(1,n,x,largestNk,dp);
     }
 };
