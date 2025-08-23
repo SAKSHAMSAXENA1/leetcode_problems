b9b1a1660672de14ed22cdfe52c20a64d0e455c2
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-23 19:36:58 +0530

    Time: 5 ms (86.43%) | Memory: 114.4 MB (68.12%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.cpp
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     vector<int> mostCompetitive(vector<int>& nums, int k) {
-        stack<int> st;
         int n=nums.size();
-        vector<int> res(k);
+        vector<int> st;
 
         for(int i=0;i<n;i++)
         {
@@ -12,21 +11,15 @@ public:
             // k-st.size() vacancies
             // after the ith element I still have n-i-1 elements to its right
             // the no of elements on the right must be >= no of vacancies after the cur posn
-            // the element at the cur posn > ith element, so displacing it with ith element
-            // gives a smaller (more competitive) result overall
-            while(!st.empty() && n-i-1 >= k-st.size() && st.top() > nums[i])
-            st.pop();
+            // the original element at the cur posn > ith element, so displacing it with 
+            // ith element gives a smaller (more competitive) result overall
+            while(!st.empty() && n-i-1 >= k-st.size() && st.back() > nums[i])
+            st.pop_back();
 
             if(st.size() < k) // I still need more elements
-            st.push(nums[i]);
+            st.push_back(nums[i]);
         }
 
-        for(int index=k-1;index>=0;index--)
-        {
-            res[index]=st.top();
-            st.pop();
-        }
-
-        return res;
+        return st;
     }
 };
