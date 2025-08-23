219b10f169980f7d4c2fa60f117cd67dcc0cbf53
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-23 22:40:54 +0530

    Time: 15 ms (14.09%) | Memory: 17.4 MB (6.84%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+/* // Memoization
 int f(int i,int farthest,vector<int> &nums1,unordered_map<int,vector<int>> &indicesNums2,int &m
 ,vector<vector<int>> &dp)
 {
@@ -32,7 +33,40 @@ int f(int i,int farthest,vector<int> &nums1,unordered_map<int,vector<int>> &indi
         }
 
         return f(0,0,nums1,indicesNums2,m,dp);
+    }*/
 
+// Tabulation
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,vector<int>> indicesNums2;
+        int m=nums1.size(),n=nums2.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+2,0));
 
+        for(int i=0;i<n;i++)
+        {
+            indicesNums2[nums2[i]].push_back(i+1);
+        }
+
+        for(int i=m-1;i>=0;i--)
+        {
+            for(int farthest=n;farthest>=0;farthest--)
+            {
+                int take=0;
+
+                for(auto it:indicesNums2[nums1[i]])
+                {
+                    if(it>farthest)
+                    {
+                        take=max(take,1+dp[i+1][it]);
+                    }
+                }
+
+                int notTake=dp[i+1][farthest];
+
+                dp[i][farthest]=max(take,notTake);
+            }
+        }
+
+        return dp[0][0];
     }
+
 };
