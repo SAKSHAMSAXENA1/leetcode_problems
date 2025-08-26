b22402517e8c7c6da45e34854f22cfaed7c5ff1f
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-26 23:50:29 +0530

    Time: 31 ms (32.25%) | Memory: 64.3 MB (11.83%) - LeetSync

--- 3391-maximum-difference-score-in-a-grid/maximum-difference-score-in-a-grid.cpp
+++ 3391-maximum-difference-score-in-a-grid/maximum-difference-score-in-a-grid.cpp
@@ -4,29 +4,21 @@ public:
         int m=grid.size(),n=grid[0].size(),res=INT_MIN;
         vector<vector<int>> maxElement(m,vector<int>(n));
 
-        maxElement[m-1][n-1]=grid[m-1][n-1];
-
-        for(int j=n-2;j>=0;j--)
-        maxElement[m-1][j]=max(maxElement[m-1][j+1],grid[m-1][j+1]);
-
-        for(int i=m-2;i>=0;i--)
-        maxElement[i][n-1]=max(maxElement[i+1][n-1],grid[i+1][n-1]);
-
-        for(int i=m-2;i>=0;i--)
+        for(int i=m-1;i>=0;i--)
         {
-            for(int j=n-2;j>=0;j--)
-            maxElement[i][j]=max({maxElement[i+1][j],grid[i+1][j],maxElement[i][j+1],grid[i][j+1]});
-        }
-
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
+            for(int j=n-1;j>=0;j--)
             {
                 if(i==m-1 && j==n-1) continue;
 
-                res=max(res,maxElement[i][j]-grid[i][j]);
+                if(i+1<m)
+                maxElement[i][j]=max({maxElement[i+1][j],grid[i+1][j]});
 
+                if(j+1<n)
+                maxElement[i][j]=max({maxElement[i][j],maxElement[i][j+1],grid[i][j+1]});
+
+                res=max(res,maxElement[i][j]-grid[i][j]);
             }
+            
         }
 
         return res;
