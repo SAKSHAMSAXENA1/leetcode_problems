52ff947211c2d46de30b9203e3e9d6c17c4cda7f
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-08-26 20:52:44 +0530

    Time: 140 ms (70.56%) | Memory: 148.9 MB (15.58%) - LeetSync

--- 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
+++ 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
@@ -4,10 +4,10 @@ public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n=nums.size();
         ll maxi=LLONG_MIN;
-        unordered_map<int,ll> numToMinPreviousPrefixSum;
+        unordered_map<int,ll> mp;
         vector<ll> prefixSum(n);
         prefixSum[0]=nums[0];
-        numToMinPreviousPrefixSum[nums[0]]=0;
+        mp[nums[0]]=0;
 
         for(int i=1;i<n;i++)
         prefixSum[i]=prefixSum[i-1]+nums[i];
@@ -16,21 +16,21 @@ public:
         {
             int a=nums[j]+k,b=nums[j]-k;
 
-            if(numToMinPreviousPrefixSum.find(a)!=numToMinPreviousPrefixSum.end())
+            if(mp.find(a)!=mp.end())
             {
-                maxi=max(maxi,prefixSum[j] - numToMinPreviousPrefixSum[a]);
+                maxi=max(maxi,prefixSum[j] - mp[a]);
             }
 
-            if(numToMinPreviousPrefixSum.find(b)!=numToMinPreviousPrefixSum.end())
+            if(mp.find(b)!=mp.end())
             {
-                maxi=max(maxi,prefixSum[j] - numToMinPreviousPrefixSum[b]);
+                maxi=max(maxi,prefixSum[j] - mp[b]);
             }
 
-            if(numToMinPreviousPrefixSum.find(nums[j])==numToMinPreviousPrefixSum.end())
-            numToMinPreviousPrefixSum[nums[j]]=prefixSum[j-1];
+            if(mp.find(nums[j])==mp.end())
+            mp[nums[j]]=prefixSum[j-1];
             else
-            numToMinPreviousPrefixSum[nums[j]]=
-            min(numToMinPreviousPrefixSum[nums[j]],prefixSum[j-1]); // min(existing val,cur val)
+            mp[nums[j]]=
+            min(mp[nums[j]],prefixSum[j-1]); // min(existing val,cur val)
         }
 
         if(maxi==LLONG_MIN) return 0;
