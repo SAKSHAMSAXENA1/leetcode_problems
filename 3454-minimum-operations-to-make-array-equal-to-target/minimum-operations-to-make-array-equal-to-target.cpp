06f9a59ea0a31539db1485ca7d610ee3da0f6ad7
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-09-01 18:42:29 +0530

    Time: 5 ms (46.08%) | Memory: 182.3 MB (44.71%) - LeetSync

--- 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
+++ 3454-minimum-operations-to-make-array-equal-to-target/minimum-operations-to-make-array-equal-to-target.cpp
@@ -4,20 +4,21 @@ public:
     long long minimumOperations(vector<int>& nums, vector<int>& target) {
         ll res=0;
         int n=nums.size();
-        vector<int> diff(n,0);
-
-        for(int i=0;i<n;i++)
-        diff[i]=target[i]-nums[i];
 
         for(int i=n-1;i>=0;i--)
         {
-            int cur=diff[i];
-            if(i>0) cur=cur-diff[i-1]; // offset from prev element
+            int curDiff=target[i]-nums[i]; // diff[i]
+            int offset=curDiff;
+            if(i>0)  
+            {
+                int prevDiff=target[i-1]-nums[i-1]; // diff[i-1]
+                offset=offset-prevDiff; // offset from prev element
+            }
 
-            if((cur<0 && diff[i]>0) || (diff[i]<0 && cur>0)) // offset to restore, ignore
+            if((offset<0 && curDiff>0) || (curDiff<0 && offset>0)) // offset to restore, ignore
             continue;
 
-            res+=(ll)min(abs(diff[i]),abs(cur));
+            res+=(ll)min(abs(curDiff),abs(offset));
         }
 
         return res;
