cd17fedb19b13f5832f64d179bb92135f4ab0f06
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2025-09-06 20:13:05 +0530

    Time: 4 ms (62.05%) | Memory: 32.5 MB (91.89%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-        int cnt=0,n=points.size();
+        int n=points.size(),cnt=0;
+
+        auto comp=[&](vector<int> &a,vector<int> &b){
+            if(a[0]==b[0]) // same x-coord
+            return a[1] > b[1]; // higher y-coord first
+
+            // x-coord diff
+            return a[0] < b[0]; // lower x-coord first
+        };
+
+        sort(points.begin(),points.end(),comp);
 
         for(int i=0;i<n;i++)
         {
             int x1=points[i][0],y1=points[i][1]; // point A
-            for(int j=0;j<n;j++)
-            {
-                if(i==j) continue;
 
+            int yMax=INT_MIN;
+            for(int j=i+1;j<n;j++)
+            {
                 int x2=points[j][0],y2=points[j][1]; // point B
 
-                if(x1<=x2 && y2<=y1) // A and B form a rectangle
+                // x1<=x2 automatically since points is sorted
+
+                if(y2 <= y1) // A & B form a rectangle
                 {
-                    bool pointInsideRectangle=false;
-
-                    for(int k=0;k<n;k++)
+                    if(yMax < y2) // no pt inside rectangle made by A and B
                     {
-                        if(k==i || k==j) continue;
-
-                        int x3=points[k][0],y3=points[k][1]; // point C
-
-                        if(x1<=x3 && x3<=x2 && y2<=y3 && y3<=y1) // pt C lies inside rectangle
-                        {
-                            pointInsideRectangle=true;
-                            break;
-                        }
+                        cnt++;
+                        yMax=y2;
                     }
-
-                    if(!pointInsideRectangle)
-                    cnt++;
                 }
             }
         }
 
-    return cnt;
+        return cnt;
     }
 };
