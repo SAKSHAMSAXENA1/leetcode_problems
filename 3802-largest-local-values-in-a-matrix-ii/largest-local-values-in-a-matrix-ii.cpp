a176720e0837dfdac6b85a9813a9cabaf9a57112
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2026-05-17 18:00:30 +0530

    Time: 1105 ms (13.15%) | Memory: 44.8 MB (94.23%) - LeetSync

---
+++ 3802-largest-local-values-in-a-matrix-ii/largest-local-values-in-a-matrix-ii.cpp
@@ -0,0 +1,88 @@
+class Seg{
+  vector<int> seg;
+public:
+    Seg(vector<int> &nums,int &n)
+    {
+        seg.resize(4*n);
+        build(0,0,n-1,nums);
+    }
+
+    void build(int idx,int low,int high,vector<int> &nums)
+    {
+        if(low==high)
+        {
+            seg[idx] = nums[low];
+            return;
+        }
+
+        int mid = (low+high)/2;
+
+        build(2*idx+1,low,mid,nums);
+        build(2*idx+2,mid+1,high,nums);
+
+        seg[idx] = max(seg[2*idx+1],seg[2*idx+2]);
+    }
+
+    int query(int idx,int low,int high,int l,int r)
+    {
+        if(high<l || r<low) return INT_MIN;
+
+        if(l<=low && high<=r) return seg[idx];
+
+        int mid = (low+high)/2;
+
+        int left = query(2*idx+1,low,mid,l,r);
+        int right = query(2*idx+2,mid+1,high,l,r);
+
+        return max(left,right);
+    }
+};
+class Solution {
+public:
+    int countLocalMaximums(vector<vector<int>>& matrix) {
+        int m=matrix.size(),n=matrix[0].size(),res=0;
+        vector<Seg> sgt;
+
+        for(int i=0;i<m;i++)
+            {
+                sgt.emplace_back(matrix[i],n);
+            }
+
+        for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                    {
+                        int x = matrix[i][j];
+
+                        if(x==0) continue;
+
+                        cout<<x<<" : ";
+                        int mx=x;
+
+                        int s=i-x,e=i+x;
+
+                        for(int row=s;row<=e;row++)
+                            {
+                                if(row<0 || row>=m) continue;
+                                
+                                int l = j-x + (row==s || row==e);
+                                l=max(l,0);
+                                int r = j+x - (row==s || row==e);
+                                r=min(r,n-1);
+
+                                if(r<l) continue;
+                                int temp = sgt[row].query(0,0,n-1,l,r);
+                                mx = max(mx,temp);
+                                //cout<<row<<" ["<<l<<","<<r<<"] "<<temp<<" , "<<endl;
+                            }
+
+                        //cout<<mx<<endl;
+                        //cout<<endl;
+
+                        res+= mx==x;
+                    }
+            }
+
+        return res;
+    }
+};
