83b426843f381c8ff06a989524b7982970534709
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2026-05-26 00:36:43 +0530

    Time: 71 ms (6.37%) | Memory: 31.2 MB (17.55%) - LeetSync

--- 2001-jump-game-vii/jump-game-vii.cpp
+++ 2001-jump-game-vii/jump-game-vii.cpp
@@ -1,29 +1,98 @@
 class Solution {
 public:
+int lb(vector<int> &vec,int key,int &m)
+{
+    int l = 0, h = m-1, res = m;
+
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+
+        if(key <= vec[mid])
+        {
+            res = mid;
+            h=mid-1;
+        }
+        else // vec[mid] < key
+        l=mid+1;
+    }
+
+    return res;
+}
+
+int ub(vector<int> &vec,int key,int &m)
+{
+    int l = 0, h = m-1, res = m;
+
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+
+        if(key < vec[mid])
+        {
+            res = mid;
+            h=mid-1;
+        }
+        else // vec[mid] <= key
+        l=mid+1;
+    }
+
+    return res;
+}
     bool canReach(string s, int minJump, int maxJump) {
-        int n=s.size(),largestJ=0;
+        if(s.back() == '1') return false;
+
+        int n = s.size(),m=0,prevFarthest = -1;
+        vector<int> zeros;
+
+        for(int i =0;i<n;i++)
+        {
+            if(s[i]=='0')
+            {
+                zeros.push_back(i);
+                m++;
+            }
+        }
+
+        /*for(int i=0;i<m;i++)
+        cout<<i<<" ";
+
+        cout<<endl;
+
+        for(auto &i:zeros)
+        cout<<i<<" ";
+
+        cout<<endl;*/
+
+        vector<bool> vis(m,false);
         queue<int> q;
         q.push(0);
+        vis[0]  = true;
 
         while(!q.empty())
         {
-            int cur=q.front();
+            int cur = q.front();
             q.pop();
 
             if(cur==n-1) return true;
 
-            int start=max(cur+minJump,largestJ+1);
+            int l = max(lb(zeros,cur+minJump,m),prevFarthest+1);
+            int r = ub(zeros,cur+maxJump,m) - 1;
 
-            int end=min(cur+maxJump,n-1);
-            for(int j=start;j<=end;j++)
+           // cout<<cur<<" : "<<l<<" to "<<r<<" -> ";
+
+            for(int j = l;j<=r;j++)
             {
-                if(s[j]=='0')
+                if(!vis[j])
                 {
-                    q.push(j);
+                    q.push(zeros[j]);
+                    vis[j] = true;
                 }
             }
 
-            largestJ=max(largestJ,end);
+            prevFarthest = r;
+
+           // cout<<endl;
         }
 
         return false;
