f06f658239d19badba26850c6fa3cb3f1fd6b86d
SAKSHAMSAXENA1 <79861670+SAKSHAMSAXENA1@users.noreply.github.com>
2026-05-29 01:08:24 +0530

    Time: 43 ms (19.44%) | Memory: 105.2 MB (8.41%) - LeetSync

--- 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.cpp
+++ 1918-maximum-score-of-a-good-subarray/maximum-score-of-a-good-subarray.cpp
@@ -25,10 +25,7 @@ public:
             nse[i]=(st.empty()) ? n : st.top();
 
             st.push(i);
-        }
 
-        for(int i =0;i<n;i++)
-        {
             if(pse[i]<k && k<nse[i])
             res=max(res,nums[i]*(nse[i]-1-pse[i]));
         }
